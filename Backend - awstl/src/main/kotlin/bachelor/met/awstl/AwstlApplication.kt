7b3d8aec8aa793d754eaa7e19245d5528de354a0
s341833 <56063595+s341833@users.noreply.github.com>
2022-01-21 10:16:05 +0100

    initial commit

---
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/AwstlApplication.kt
@@ -0,0 +1,11 @@
+package bachelor.met.awstl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwstlApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwstlApplication>(*args)
+}
---
+++ Backend - awstl/src/test/kotlin/bachelor/met/awstl/AwstlApplicationTests.kt
@@ -0,0 +1,13 @@
+package bachelor.met.awstl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwstlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
