7f1db35c0e59ccf0b16db93093d14907e175dd44
Erik Myhre <myherik@gmail.com>
2022-01-31 19:02:21 +0100

    hello_world_3.0

--- Frontend - awstl/src/App.tsx
+++ Frontend - awstl/src/App.tsx
@@ -1,36 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axio from 'axios';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import Testlist from './Testlist';
+import Hello from './Hello';
 
 function App() {
-
-  const [melding, setMelding] = useState("")
-
-  axios.get("/api/hei")
-  .then(response => {
-    console.log(response)
-    setMelding(response.data);
-  })
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {melding}
-        </a>
-      </header>
-    </div>
+    <>
+      <Hello />
+      <Testlist />
+    </>
   );
 }
 
