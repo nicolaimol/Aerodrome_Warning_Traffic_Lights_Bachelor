732e81379e784efa259d08a0d2b422faacb82407
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-08 20:52:17 +0100

    added secret for redis password

--- VM/secrets.sh
+++ VM/secrets.sh
@@ -4,3 +4,4 @@ kubectl create secret generic mysql-root-pass --from-literal=password=password
 kubectl create secret generic mysql-user-pass --from-literal=username=user
 kubectl create secret generic mysql-db-url --from-literal=database=awtl
 kubectl create secret generic mysql-usr-pass --from-literal=password=userpassword
+kubectl create secret generic redis-pass --from-literal=password=redispassword
