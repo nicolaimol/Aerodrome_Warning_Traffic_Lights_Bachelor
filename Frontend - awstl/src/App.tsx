82c00862e4a790c19b5e737a4321ee6ea9c5900c
Erik Myhre <myherik@gmail.com>
2022-02-09 10:20:04 +0100

    banner?

--- Frontend - awstl/src/App.tsx
+++ Frontend - awstl/src/App.tsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import axio from 'axios';
 import './App.css';
-import axios from 'axios';
 import Testlist from './components/Testlist';
 import Hello from './components/Hello';
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Banner />
       <Hello />
       <Testlist />
     </>
