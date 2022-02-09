6e1112a5b67f527865f3ddcba7259200871dea30
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-09 22:08:02 +0100

    redux reducer weather

---
+++ Frontend - awstl/src/Reducers/weather.ts
@@ -0,0 +1,21 @@
+import IWeatherAction from "../model/weatherAction"
+
+const weather = (state = {}, action: IWeatherAction) => {
+
+    switch (action.type) {
+        case "SET_WEATHER": 
+            return {
+                ...state,
+                value: action.payload
+            }
+        case "CLEAR_WEATHER":
+            return {
+                ...state,
+                value: {}
+            }
+        default:
+            return state
+    }
+}
+
+export default weather;
