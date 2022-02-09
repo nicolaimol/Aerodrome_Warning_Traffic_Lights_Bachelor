9e3b8dde4f8765a79c5f57fbfe3d48ba27d10daa
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-09 22:09:40 +0100

    redux reducer input

---
+++ Frontend - awstl/src/Reducers/input.ts
@@ -0,0 +1,22 @@
+import iInputAction from "../model/inputAction";
+
+const input = (state = {input1: "", input2: "", input3: ""}, action: iInputAction) => {
+    switch (action.type) {
+        case "SET_INPUT": 
+            return {
+                ...state,
+                value: action.payload
+            }
+        case "CLEAR_INPUT":
+            return {
+                ...state,
+                value: {input1: "", input2: "", input3: ""}
+            }
+        default:
+            return {
+                ...state
+            }
+    }
+}
+
+export default input;
