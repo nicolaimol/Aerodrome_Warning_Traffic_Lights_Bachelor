b84c707fbe41bcf5c400e9b40bd277e42bf54971
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-09 22:06:03 +0100

    combine actions

---
+++ Frontend - awstl/src/Actions/index.ts
@@ -0,0 +1,9 @@
+import inputActions from "./inputActions";
+import weatherActions from "./weatherActions";
+
+const allActions = {
+    inputActions,
+    weatherActions
+}
+
+export default allActions
