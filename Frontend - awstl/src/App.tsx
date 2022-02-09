55600fa3fe4dfd0c46974ba75fb8e8c992558be5
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-09 22:05:02 +0100

    setup for routing

--- Frontend - awstl/src/App.tsx
+++ Frontend - awstl/src/App.tsx
@@ -7,28 +7,26 @@ import Banner from './components/Banner';
 import Testinputs from './components/Testinputs';
 import axios from 'axios';
 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import allActions from './Actions'
+import ShowInput from './components/ShowInput';
+
 
 function App() {
 
-  interface inputs {
-    input1: string;
-    input2: string;
-    input3: string;
-  }
+  const dispatch = useDispatch();
 
   useEffect(() => {
     axios.get('/api/input')
     .then((response) => {
       console.log(response);
+      dispatch(allActions.inputActions.setInput(response.data));
     })
   })
 
+/*
 
-
-
-  return (
-    <>
-      <Navbar />
       <Banner />
       {<div className="inputs">
         <Testinputs />
@@ -36,7 +34,21 @@ function App() {
       
       <Hello />
       <Testlist />
+*/
+
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/list" element={<Testlist />} />
+        <Route path="/input" element={<Testinputs />} />
+        <Route path="/vis" element={<ShowInput />} />
+        <Route path="/" element={<Hello />} />
+      </Routes>
     </>
+
+      
   );
 }
 
