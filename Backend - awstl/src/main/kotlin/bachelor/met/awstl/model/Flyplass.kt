9f54d3a819f50d855d4c8e91bcd9651fafc19daa
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-14 09:31:27 +0100

    new fields and constructor

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/model/Flyplass.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/model/Flyplass.kt
@@ -1,27 +1,36 @@
 package bachelor.met.awstl.model
 
+import java.io.Serializable
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity
-class Flyplass() {
+class Flyplass(): Serializable {
 
     @Id
-    lateinit var id: String
+    lateinit var icao: String
 
     lateinit var navn: String
+    var iata: String? = null
     lateinit var altitude: String
     lateinit var lat: String
     lateinit var lon: String
 
     lateinit var rwy: String
 
-    constructor(id: String, navn: String, altitude: String, lat: String, lon: String, rwy: String) : this() {
-        this.id = id
+    constructor(icao: String, navn: String, iata: String, altitude: String, lat: String, lon: String, rwy: String): this() {
+        this.icao = icao
         this.navn = navn
+        this.iata = iata
         this.altitude = altitude
         this.lat = lat
         this.lon = lon
         this.rwy = rwy
     }
+
+    override fun toString(): String {
+        return "Flyplass(icao='$icao', navn='$navn', iata=$iata, altitude='$altitude', lat='$lat', lon='$lon', rwy='$rwy')"
+    }
+
+
 }
