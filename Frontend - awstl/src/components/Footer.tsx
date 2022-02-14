fbdad21b2e3733e65dc596174f643a526758caf3
Erik Myhre <myherik@gmail.com>
2022-02-14 11:52:59 +0100

    new interfaces

---
+++ Frontend - awstl/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BottomNavigation, BottomNavigationAction, Box, Typography } from '@mui/material'
+
+function Footer() {
+  return (
+    <Box>
+      <BottomNavigation sx={{ backgroundColor: '#496c80', height: 200 }}>
+        <Typography
+        variant= 'h6'
+        sx={{ color: 'white'}}>
+            Dette er en Footer
+        </Typography>
+      </BottomNavigation>
+    </Box>
+  )
+}
+
+export default Footer
---
+++ Frontend - awstl/src/model/flyplassInfo.ts
@@ -0,0 +1,11 @@
+
+interface flyplassInfo {
+    navn: string;
+    beskrivelse: string;
+    temperatur: number;
+    styrke: string;
+    retning: string;
+    ikonNavn: string;
+}
+
+export default flyplassInfo;
