c494e9b75a49022415bbb2086cca8f3ba9b72817
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-16 14:21:14 +0100

    controller for airport, regular and dto get

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/controller/FlyplassController.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/controller/FlyplassController.kt
@@ -1,4 +1,26 @@
 package bachelor.met.awstl.controller
 
-class FlyplassController {
+import bachelor.met.awstl.mapper.FlyplassToFlyplassDto
+import bachelor.met.awstl.service.FlyplassService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = ["/api"])
+@CrossOrigin(value = ["http://localhost:3001"])
+class FlyplassController(var service: FlyplassService) {
+
+    @GetMapping(value = ["/airport/dto"])
+    fun getAllAirportsDto(): ResponseEntity<Any> {
+        return ResponseEntity.ok(service.getAllFlyplass().map { FlyplassToFlyplassDto.convert(it) })
+    }
+
+    @GetMapping(value = ["/airport"])
+    fun getAllAirports(): ResponseEntity<Any> {
+        return ResponseEntity.ok(service.getAllFlyplass())
+    }
+
 }
