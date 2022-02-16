18001796c919926c8cf845c7ca67e1fd1d944414
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-16 12:36:40 +0100

    nowcast reducer added

--- Frontend - awstl/src/Reducers/index.ts
+++ Frontend - awstl/src/Reducers/index.ts
@@ -1,10 +1,12 @@
 import input from "./input";
 import weather from "./weather";
 import { combineReducers } from 'redux'
+import nowcast from "./nowcast";
 
 export const rootReducer = combineReducers({
     input,
-    weather
+    weather,
+    nowcast
 })
 
 export type RootState = ReturnType<typeof rootReducer>
