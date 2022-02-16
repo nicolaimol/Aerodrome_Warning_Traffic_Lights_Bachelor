52d513a0744015122dea07e9bdbffad9b9869236
Erik Myhre <myherik@gmail.com>
2022-02-16 13:18:19 +0100

    la til banner slik at den vises

--- Frontend - awstl/src/pages/Hjem.tsx
+++ Frontend - awstl/src/pages/Hjem.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@mui/material'
 import React from 'react'
+import Banner from '../components/Banner'
 import Arbeider from '../components/Arbeider'
 import RaskVaer from '../components/RaskVaer'
 
 function Hjem() {
   return (
     <>
+    <Banner />
     <Container>
         <Arbeider />
     </Container>
