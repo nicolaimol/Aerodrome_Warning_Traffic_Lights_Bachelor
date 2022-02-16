916c6c65f7a4a4b20c2a93c4d727392921dfbd96
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-16 10:10:14 +0100

    nowcast suports all airport, individual airport cache

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/service/NowcastService.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/service/NowcastService.kt
@@ -3,12 +3,17 @@ package bachelor.met.awstl.service
 import bachelor.met.awstl.dto.NowcastDto
 import bachelor.met.awstl.dto.nowcast.Nowcast
 import bachelor.met.awstl.mapper.FlyplassToFlyplassDto
+import bachelor.met.awstl.model.Flyplass
 import bachelor.met.awstl.repo.IFlyplassRepo
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForObject
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 @Service
 class NowcastService(val restTemplate: RestTemplate, val repo: IFlyplassRepo) {
@@ -19,41 +24,73 @@ class NowcastService(val restTemplate: RestTemplate, val repo: IFlyplassRepo) {
     val logger = LoggerFactory.getLogger(NowcastService::class.java)
 
 
-    @Cacheable(value = ["nowcast"], key = "#icao")
     fun getNowcast(icao: String): NowcastDto {
         val dto = NowcastDto();
-        val stdFlyplasser = arrayOf("ENGM", "ENBR", "ENVA")
-        if (icao in stdFlyplasser) {
-            throw Exception("Not implemented yet")
+        val stdFlyplasser = arrayListOf("ENGM", "ENBR", "ENVA")
+        if (icao.uppercase() in stdFlyplasser) {
+
+            stdFlyplasser.removeAt(stdFlyplasser.indexOf(icao))
+
+            stdFlyplasser.add(0, icao)
+            stdFlyplasser.add("ENZV")
+
+
+            logger.info("henter nowcast for $icao standard")
+            val airports = arrayOf(
+                repo.findById(icao.uppercase()),
+                repo.findById(stdFlyplasser[1]),
+                repo.findById(stdFlyplasser[2]),
+                repo.findById(stdFlyplasser[3]))
+
+            getWeather(airports, dto)
+
+            return dto
+
+
+
         } else {
             logger.info("henter nowcast for $icao med standard")
             val airports = arrayOf(
-                repo.findById(icao),
-                repo.findById("engm"),
-                repo.findById("enbr"),
-                repo.findById("enva"))
+                repo.findById(icao.uppercase()),
+                repo.findById("ENGM"),
+                repo.findById("ENBR"),
+                repo.findById("ENVA"))
 
-            for (airport in airports) {
-                val query = HashMap<String, String>()
-                query["altitude"] = airport.get().altitude
-                query["lat"] = airport.get().lat
-                query["lon"] = airport.get().lon
-
-                try {
-                    restTemplate.getForObject(url, Nowcast::class.java, query)?.let {
-                        it.properties.timeseries = it.properties.timeseries.copyOfRange(0, 1) ;
-                        dto.nowcasts.add(it) }
-                    dto.airports.add(FlyplassToFlyplassDto.convert(airport.get()))
-                } catch (e: Exception) {
-                    logger.error(e.message)
-                    e.printStackTrace()
-                    throw Exception("something went wrong")
-                }
-            }
+            getWeather(airports, dto)
 
             return dto
         }
 
+    }
+
+    private fun getWeather(
+        airports: Array<Optional<Flyplass>>,
+        dto: NowcastDto
+    ) {
+        for (airport in airports) {
+            val query = HashMap<String, String>()
+            query["altitude"] = airport.get().altitude
+            query["lat"] = airport.get().lat
+            query["lon"] = airport.get().lon
+
+            try {
+                getForAirport(airport.get().icao, query)?.let {
+                    it.properties.timeseries = it.properties.timeseries.copyOfRange(0, 1);
+                    dto.nowcasts.add(it)
+                }
+                dto.airports.add(FlyplassToFlyplassDto.convert(airport.get()))
+            } catch (e: Exception) {
+                logger.error(e.message)
+                e.printStackTrace()
+                throw Exception("something went wrong")
+            }
+        }
+    }
+
+    @Cacheable(value = ["nowcast"], key = "#icao")
+    fun getForAirport(icao: String, query: HashMap<String, String>): Nowcast? {
+        logger.info("Nowcast for $icao")
+        return restTemplate.getForObject(url, Nowcast::class.java, query)
     }
 
 
