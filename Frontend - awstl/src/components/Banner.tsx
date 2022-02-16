2f948b9e86f74022d783bbd0d1d50a680bb0fc92
Erik Myhre <myherik@gmail.com>
2022-02-16 20:23:18 +0100

    Styling til autocomplete

--- Frontend - awstl/src/components/Banner.tsx
+++ Frontend - awstl/src/components/Banner.tsx
@@ -3,11 +3,43 @@ import Typography from '@mui/material/Typography';
 import { Autocomplete, Container, TextField } from '@mui/material';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import flyplasser from '../model/flyplasser'
+import flyplasser from '../model/flyplasser';
+
+import { createStyles, makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    
+    "& .MuiInputLabel-outlined:not(.MuiInputLabel-shrink)": {
+      
+      //color: '#0090a8'
+    },
+
+    '& .MuiInputLabel-outlined': { color: '#0090a8' }
+  },
+  inputRoot: {
+    color: "#0090a8",
+
+    '&[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-child': {
+      
+    },
+    "& .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#0090a8"
+    },
+    "&:hover .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#0494ac"
+    },
+    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#496c80"
+    }
+  }
+}));
 
 export default function Banner() {
 
   const [flyplasserList, setFlyplasserList] = useState<flyplasser[]>([]);
+
+  const classes = useStyles();
 
   useEffect(() => {
       axios.get('http://localhost:8080/api/airport')
@@ -33,14 +65,15 @@ export default function Banner() {
           <Typography sx={{ pt: 5}} component="h1" variant="h3" color="inherit" gutterBottom>
             Aerodrome Warning Traffic Light System
           </Typography>
-          <div>
+          <div className='AutoCompleteCustom'>
             <Autocomplete
             disablePortal
             id="combo-box-flyplasser"
+            classes={classes}
             options={relevantFlyplassData.sort((a, b) => -b.label.charAt(0).toString().localeCompare(a.label.charAt(0).toString()))}
             groupBy={(relevantFlyplassData) => relevantFlyplassData.label.charAt(0).toString()}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Flyplass" />}
+            sx={{ width: 300, backgroundColor: '#FFFFFF'}}
+            renderInput={(params) => <TextField {...params} style={{ backgroundColor: "pink !important" }} label="Velg flyplass" />}
           />
           </div>
           
