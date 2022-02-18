de924ac0dbbc22194724bb132c6c526364ef12fd
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-18 14:32:23 +0100

    added airport action

--- Frontend - awstl/src/Reducers/index.ts
+++ Frontend - awstl/src/Reducers/index.ts
@@ -2,11 +2,13 @@ import input from "./input";
 import weather from "./weather";
 import { combineReducers } from 'redux'
 import nowcast from "./nowcast";
+import airport from './airport'
 
 export const rootReducer = combineReducers({
     input,
     weather,
-    nowcast
+    nowcast,
+    airport
 })
 
 export type RootState = ReturnType<typeof rootReducer>
