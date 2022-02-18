121d598979ac9ba56e068ade2851931dbfb854af
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-18 14:53:51 +0100

    removed part of code, error in console

--- Frontend - awstl/src/components/Footer.tsx
+++ Frontend - awstl/src/components/Footer.tsx
@@ -5,11 +5,13 @@ function Footer() {
   return (
     <Box>
       <BottomNavigation sx={{ backgroundColor: '#496c80', height: 200 }}>
+        {/*
         <Typography
         variant= 'h6'
         sx={{ color: 'white'}}>
             Dette er en Footer
         </Typography>
+        */}
       </BottomNavigation>
     </Box>
   )
