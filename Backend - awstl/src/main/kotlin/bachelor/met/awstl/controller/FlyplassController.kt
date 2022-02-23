96d495f3bc6188b316dbce461ad6f4129a838744
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-23 12:47:55 +0100

    Comments added

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/controller/FlyplassController.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/controller/FlyplassController.kt
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+
+/**
+ * Controller for receiving data about airports
+ * Available endpoints
+ *  /api/airport
+ *  /api/airport/dto
+ * dto endpoint converts data to dto to reduce received data
+ *
+ * logic to get data is done in injected FlyplassService
+ */
 @RestController
 @RequestMapping(value = ["/api"])
 @CrossOrigin(value = ["http://localhost:3001"])
