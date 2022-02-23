d45add5578a63ef35454ce15f4a80cc876d97276
Erik Myhre <myherik@gmail.com>
2022-02-23 13:13:14 +0100

    viser komponent på siden med manuellt innsatte props verdier

--- Frontend - awstl/src/pages/Trafikklys.tsx
+++ Frontend - awstl/src/pages/Trafikklys.tsx
@@ -11,10 +11,24 @@ function Trafikklys() {
     <Container>
       {/*
       <div style={{ display: 'flex', justifyContent: 'space-evenly', flexFlow: 'row wrap', alignItems: 'center'}}>
-        <VisSatteTerskelverdier />
+        
         <TrafikklysBox />
       </div>
       */}
+      <VisSatteTerskelverdier 
+      airTemp={0}
+      precipitationAmmount={20}
+      windSpeed={30}
+      windDirection={180}
+      windGust={40}
+      probThunder={50}
+      dewpoint={4}
+      humidity={2}
+      fog={40}
+
+      probIce={20}
+      crosswind={50}
+      />
       <div>
         <Tidslinje />
       </div>
