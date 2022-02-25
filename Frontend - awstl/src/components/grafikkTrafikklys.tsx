d87218667d061099faf5c05c59ef97b0e64ea3b0
Erik Myhre <myherik@gmail.com>
2022-02-25 09:56:20 +0100

    grafikk komponent

---
+++ Frontend - awstl/src/components/grafikkTrafikklys.tsx
@@ -0,0 +1,14 @@
+import { minHeight } from '@mui/system'
+import React from 'react'
+
+function grafikkTrafikklys() {
+  return (
+    <>
+    <div style={{ backgroundColor: '#dff2f6', minHeight: '20vh'}}>
+
+    </div>
+    </>
+  )
+}
+
+export default grafikkTrafikklys
