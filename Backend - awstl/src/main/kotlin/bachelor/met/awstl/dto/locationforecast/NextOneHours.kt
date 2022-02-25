06215947ace1dad3e1180a815d90e2172b5cc441
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-25 11:26:06 +0100

    locationforecast get weathericon

---
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/dto/locationforecast/NextOneHours.kt
@@ -0,0 +1,8 @@
+package bachelor.met.awstl.dto.locationforecast
+
+import java.io.Serializable
+
+class NextOneHours: Serializable {
+
+    lateinit var summary: Summary
+}
---
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/dto/locationforecast/Summary.kt
@@ -0,0 +1,8 @@
+package bachelor.met.awstl.dto.locationforecast
+
+import java.io.Serializable
+
+class Summary: Serializable {
+
+    var symbol_code: String = ""
+}
