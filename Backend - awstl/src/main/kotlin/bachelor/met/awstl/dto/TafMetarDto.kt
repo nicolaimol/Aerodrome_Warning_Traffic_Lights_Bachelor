243553934307a9b320bb1a6bfff43a77fe0f6803
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-28 14:17:41 +0100

    added constructor for taf and metar fields

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/dto/TafMetarDto.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/dto/TafMetarDto.kt
@@ -2,7 +2,12 @@ package bachelor.met.awstl.dto
 
 import java.io.Serializable
 
-class TafMetarDto: Serializable {
+class TafMetarDto(): Serializable {
+
+    constructor(taf: String, metar: String): this() {
+        this.taf = taf
+        this.metar = metar
+    }
 
     lateinit var taf: String
     lateinit var metar: String
