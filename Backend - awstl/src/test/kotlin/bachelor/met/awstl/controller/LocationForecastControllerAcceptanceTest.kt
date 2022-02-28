03832afb7bfc337dff2fbfee2ad9a7c5c1ed15c1
s341833 <56063595+s341833@users.noreply.github.com>
2022-02-28 19:44:36 +0100

    locationforcast acceptance test for valid airport, not valid airport and no param

---
+++ Backend - awstl/src/test/kotlin/bachelor/met/awstl/controller/LocationForecastControllerAcceptanceTest.kt
@@ -0,0 +1,76 @@
+package bachelor.met.awstl.controller
+
+import bachelor.met.awstl.model.Flyplass
+import bachelor.met.awstl.repo.IFlyplassRepo
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import redis.embedded.RedisServer
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class LocationForecastControllerAcceptanceTest {
+
+    @Autowired
+    val mockMvc: MockMvc? = null
+
+    @Autowired
+    var repo: IFlyplassRepo? = null
+
+    var redisServer: RedisServer? = null
+
+    @BeforeAll
+    fun setUp() {
+        try {
+            redisServer = RedisServer.builder().port(6370).build()
+            redisServer!!.start()
+        } finally {
+
+        }
+
+        val engm = Flyplass("engm", "Oslo Lufthavn, Gardermoen",
+            "OSL", "100", "60", "10", "01/19")
+
+        repo!!.save(engm)
+    }
+
+    @AfterAll
+    fun tearDown() {
+        redisServer!!.stop()
+
+        repo!!.deleteAll()
+    }
+
+    @Test
+    fun getLocationForecastSuccess() {
+        mockMvc!!.perform(MockMvcRequestBuilders
+            .get("/api/locationforecast")
+            .contentType(MediaType.APPLICATION_JSON)
+            .param("icao", "engm")
+        )
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun getLocationForecastBadRequest() {
+        mockMvc!!.perform(MockMvcRequestBuilders
+            .get("/api/locationforecast")
+            .param("icao", "test")
+            .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(content().string("Airport with icao test not found"))
+    }
+
+
+}
