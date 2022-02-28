bc45427702d195bf9d1beef4539a18140deb5453
Erik Myhre <myherik@gmail.com>
2022-02-28 14:16:38 +0100

    koffertkomponent

---
+++ Frontend - awstl/src/components/DrawerTerskelverdier.tsx
@@ -0,0 +1,73 @@
+import { Box, List, ListItem, ListItemIcon, ListItemText, Divider, Button, SwipeableDrawer } from '@mui/material';
+import InboxIcon from '@mui/icons-material/Inbox';
+import MailIcon from '@mui/icons-material/Mail';
+import React from 'react'
+
+import TerskelForm from './TerskelForm';
+
+function DrawerTerskelverdier() {
+
+    type Anchor = 'left'
+
+    const [state, setState] = React.useState({
+        left: false
+    })
+
+    const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor: Anchor) => (
+        <Box
+          sx={{ minWidth: 300, width: '50vw' }}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <div style={{ marginLeft: '25px'}}>
+                <h2>Sett terskelverdier her</h2>
+            </div>
+            <Divider />
+            <div style={{ marginLeft: '25px'}}>
+                <TerskelForm />
+            </div>
+          
+          <Divider />
+          
+        </Box>
+      );
+
+
+  return (
+    <>
+    <div>
+      {(['left'] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default DrawerTerskelverdier
