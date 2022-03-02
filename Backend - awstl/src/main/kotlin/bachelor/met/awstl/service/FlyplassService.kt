cbcbb1cf5ea351782c059986170cd6b82b5de89f
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-02 10:25:01 +0100

    optimized getFlyplass for Optional and elseThrow with new Exception

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/service/FlyplassService.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/service/FlyplassService.kt
@@ -1,6 +1,7 @@
 package bachelor.met.awstl.service
 
 import bachelor.met.awstl.dto.FlyplassDto
+import bachelor.met.awstl.exception.AirportNotFoundException
 import bachelor.met.awstl.mapper.FlyplassToFlyplassDto
 import bachelor.met.awstl.model.Flyplass
 import bachelor.met.awstl.repo.IFlyplassRepo
@@ -13,12 +14,8 @@ class FlyplassService(val repo: IFlyplassRepo) {
     @Cacheable(value = ["flyplass"], key = "#icao")
     fun getFlyplass(icao: String): Flyplass {
 
-        try {
-            return repo.getById(icao.uppercase())
-        }
-        catch (e: Exception) {
-            throw Exception("Airport with icao $icao not found")
-        }
+        return repo.findById(icao.uppercase()).orElseThrow { AirportNotFoundException("Airport with icao $icao not found")}
+
     }
 
     @Cacheable(value = ["flyplass"])
