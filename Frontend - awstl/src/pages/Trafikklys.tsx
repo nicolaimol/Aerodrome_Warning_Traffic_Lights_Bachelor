0fe1123fcceb0dabdf9ac04eafbf6376fc048e2c
Erik Myhre <myherik@gmail.com>
2022-03-04 12:31:32 +0100

    fjernet kommentar og style fix

--- Frontend - awstl/src/pages/Trafikklys.tsx
+++ Frontend - awstl/src/pages/Trafikklys.tsx
@@ -43,24 +43,9 @@ function Trafikklys() {
         </Typography>
         <Divider sx={{ mb: 5 }} />
 
-      <div style={{ display: 'flex', justifyContent: 'space-evenly', flexFlow: 'row wrap', alignItems: 'center'}}>
-        <VisSatteTerskelverdier
-            terskel={terskel !== undefined ? terskel : defaultVerdier}
-            /*
-              airTemp={0}
-              precipitationAmmount={20}
-              windSpeed={30}
-                    windDirection={180}
-              windGust={40}
-              probThunder={50}
-              dewpoint={4}
-              humidity={2}
-              fog={40}
-
-              probIce={20}
-              crosswind={50}
-              */
-      />
+      <div style={{ display: 'flex', justifyContent: 'space-evenly', flexFlow: 'row wrap', alignItems: 'stretch'}}>
+        
+        <VisSatteTerskelverdier terskel={terskel !== undefined ? terskel : defaultVerdier} />
         <TrafikklysBox farge={color} />
       </div>
       
