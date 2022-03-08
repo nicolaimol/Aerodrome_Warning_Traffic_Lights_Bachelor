7a192a0f449c1b2ada2d288277f3a20b433379b1
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-08 20:26:25 +0100

    removed unused import

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/AwstlApplication.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/AwstlApplication.kt
@@ -1,16 +1,11 @@
 package bachelor.met.awstl
 
-import org.slf4j.LoggerFactory
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.stereotype.Component
-import java.io.File
-import java.util.*
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @SpringBootApplication
 @EnableCaching
