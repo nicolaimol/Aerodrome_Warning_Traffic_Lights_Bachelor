470d8bd11cb3cb2da6f80c259c8c97476af4a993
Erik Myhre <myherik@gmail.com>
2022-03-09 12:05:37 +0100

    model tar nå min of max

--- Frontend - awstl/src/model/terskelData.ts
+++ Frontend - awstl/src/model/terskelData.ts
@@ -1,19 +1,32 @@
 interface terskelData{
-    airTemp: number; // temperatur (air_temperature)
-    precipitationAmmount: number; // nedbør (precipitation_ammount)
-    windSpeed: number; // vindstyrke (wind_speed)
-    //windDirection: number; // vindretning (wind_from_direction)
-    windGust: number; // vindkast (wind_speed_of_gust)
-    probThunder: number; // sannsynlighet torden (probability_of_thunder)
-    //dewpoint: number; // temperatur luftfuktighet kondenserer (dew_point_temperature)
-    humidity: number; // luftfuktighet (relative_humidity)
-    fog: number; // tåke (fog_area_fraction)
+    airTempMin: number; // temperatur (air_temperature)
+    airTempMax: number; // temperatur (air_temperature)
+
+    precipitationMax: number; // nedbør (precipitation_ammount)
+    precipitationMin: number; // nedbør (precipitation_ammount)
+
+    windSpeedMin: number; // vindstyrke (wind_speed)
+    windSpeedMax: number; // vindstyrke (wind_speed)
+
+        //windDirection: number; // vindretning (wind_from_direction)
     
-    probIce: number;
-    crosswind: number;
+    windGustMin: number; // vindkast (wind_speed_of_gust)
+    windGustMax: number; // vindkast (wind_speed_of_gust)
 
+    probThunderMin: number; // sannsynlighet torden (probability_of_thunder)
+    probThunderMax: number; // sannsynlighet torden (probability_of_thunder)
 
-    // VI SKAL OGSÅ HA IS% OG CROSSWIND MEN DISSE VERDIENE REGNES UT AV temp+fuktighet og v.retning+v.styrke+rullebaneretning
+    //dewpoint: number; // temperatur luftfuktighet kondenserer (dew_point_temperature)
+    
+    humidityMin: number; // luftfuktighet (relative_humidity)
+    humidityMax: number; // luftfuktighet (relative_humidity)
+
+    fog: number; // tåke (fog_area_fraction)
+
+    crosswindMin: number;
+    crosswindMax: number;
+
+    // VI SKAL OGSÅ HA IS% OG CROSSWIND MEN DISSE VERDIENE REGNES UT AV temp+fuktighet og v.retning + v.styrke + rullebaneretning
 
 }
 
