af86c6423dec97347fc1e7d7bd703d9ae3cebdd5
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-09 12:13:43 +0100

    checks for new message in response for error

--- Backend - awstl/src/test/kotlin/bachelor/met/awstl/controller/TafMetarControllerIntegrationTest.kt
+++ Backend - awstl/src/test/kotlin/bachelor/met/awstl/controller/TafMetarControllerIntegrationTest.kt
@@ -17,8 +17,7 @@ import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 @ContextConfiguration(classes = [TafMetarController::class])
@@ -83,7 +82,7 @@ internal class TafMetarControllerIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
         )
             .andExpect(status().isInternalServerError)
-            .andExpect(content().string("feil"))
+            .andExpect(jsonPath("$.message").value("feil"))
 
     }
 
