dd9716df97f67fcf403cb46df6caa9df3a2c0b1e
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-09 12:13:33 +0100

    checks for new message in response for error

--- Backend - awstl/src/test/kotlin/bachelor/met/awstl/controller/LocationForecastControllerIntegrationTest.kt
+++ Backend - awstl/src/test/kotlin/bachelor/met/awstl/controller/LocationForecastControllerIntegrationTest.kt
@@ -17,8 +17,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 @ContextConfiguration(classes = [LocationForecastController::class])
@@ -97,7 +96,7 @@ internal class LocationForecastControllerIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
         )
             .andExpect(status().isInternalServerError)
-            .andExpect(content().string("feil"))
+            .andExpect(jsonPath("$.message").value("feil"))
 
     }
 
