cd1fc5b2ecea16f18c6e621ad87d6be008a85921
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-09 14:57:41 +0100

    uses new file for slider style

--- Frontend - awstl/src/components/TerskelForm.tsx
+++ Frontend - awstl/src/components/TerskelForm.tsx
@@ -1,9 +1,12 @@
-import { Slider } from '@mui/material';
+import { Slider, Divider, Button } from '@mui/material';
 import React, { useState, useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import allActions from '../Actions';
 import * as buffer from "buffer";
 import axios from 'axios'
+
+import { defaultVerdier } from '../App'
+import { generateStyle } from '../util/sliderStyleUtil'
 
 function TerskelForm() {
 
@@ -35,9 +38,11 @@ function TerskelForm() {
         dispatch(allActions.terskelActions.setTerskel(formVerdier))
     }, [formVerdier])
 
-    useEffect(() => {
-      setFormVerdier(terskel);
-    }, [terskel])
+    const tilbakestillTerskelverdier = () => {
+        setFormVerdier(defaultVerdier)
+
+        //dispatch(allActions.terskelActions.setTerskel(defaultVerdier));
+    }
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
@@ -47,10 +52,13 @@ function TerskelForm() {
   return (
     <>
     <form onSubmit={handleSubmit}>
-      <div style={{ width: "70%" }}>
-            Lufttemperatur terskelverdi
+      <div style={{width: "calc(100% - 40px)", padding:"5px 20px", backgroundColor: "#dff2f6"}}>
+          <span style={{color: "#0090a8"}}>Lufttemperatur</span>
+          <div style={{display: 'flex', flexDirection: "row"}}>
             <Slider
-
+            classes={
+                generateStyle(formVerdier.airTempMax, 60, -60, false)
+            }
               onChange={handleSliderEndring("airTemp")}
               value={[formVerdier.airTempMin, formVerdier.airTempMax]}
               step={1}
@@ -79,12 +87,18 @@ function TerskelForm() {
                   label: "60°C",
                 },
               ]}
-              valueLabelDisplay="off"
+              valueLabelDisplay="auto"
             />
+          <p style={{marginLeft: '30px', width: 'fit-content', whiteSpace: "nowrap"}}>{`${formVerdier.airTempMin}, ${formVerdier.airTempMax}`}</p>
           </div>
-      <div style={{ width: "70%" }}>
-        Nedbør terskelverdi
+          </div>
+      <div style={{ width: "calc(100% - 40px)", padding:"5px 20px" }}>
+          <span style={{color: "#0090a8"}}>Nedbør</span>
         <Slider
+            classes={
+                generateStyle(formVerdier.precipitationMax, 5, 0, true)
+            }
+
           value={[formVerdier.precipitationMin, formVerdier.precipitationMax]}
           onChange={handleSliderEndring("precipitation")}
           step={0.1}
@@ -105,12 +119,16 @@ function TerskelForm() {
               label: "5mm",
             },
           ]}
-          valueLabelDisplay="off"
+          valueLabelDisplay="auto"
         />
       </div>
-      <div style={{ width: "70%" }}>
-        Vindfart terskelverdi
+      <div style={{ width: "calc(100% - 40px)", padding:"5px 20px", backgroundColor: "#dff2f6" }}>
+          <span style={{color: "#0090a8"}}>Vindfart</span>
         <Slider
+            classes={
+                generateStyle(formVerdier.windSpeedMax, 60, 0, true)
+            }
+
           value={[formVerdier.windSpeedMin, formVerdier.windSpeedMax]}
           onChange={handleSliderEndring("windSpeed")}
           step={1}
@@ -131,12 +149,16 @@ function TerskelForm() {
               label: "60 kt",
             },
           ]}
-          valueLabelDisplay="off"
+          valueLabelDisplay="auto"
         />
       </div>
-      <div style={{ width: "70%" }}>
-        Vindkast terskelverdi
+      <div style={{ width: "calc(100% - 40px)", padding:"5px 20px" }}>
+          <span style={{color: "#0090a8"}}>Vindkast</span>
         <Slider
+            classes={
+                generateStyle(formVerdier.windGustMax, 60, 0, true)
+            }
+
           value={[formVerdier.windGustMin, formVerdier.windGustMax]}
           onChange={handleSliderEndring("windGust")}
           step={1}
@@ -157,12 +179,16 @@ function TerskelForm() {
               label: "60 kt",
             },
           ]}
-          valueLabelDisplay="off"
+          valueLabelDisplay="auto"
         />
       </div>
-      <div style={{ width: "70%" }}>
-        Sannsynlighet torden terskelverdi
+      <div style={{ width: "calc(100% - 40px)", padding:"5px 20px", backgroundColor: "#dff2f6" }}>
+          <span style={{color: "#0090a8"}}>Sannsynlighet torden</span>
         <Slider
+            classes={
+                generateStyle(formVerdier.probThunderMax, 100, 0, true)
+            }
+
           value={[formVerdier.probThunderMin, formVerdier.probThunderMax]}
           onChange={handleSliderEndring("probThunder")}
           step={1}
@@ -183,12 +209,16 @@ function TerskelForm() {
               label: "100%",
             },
           ]}
-          valueLabelDisplay="off"
+          valueLabelDisplay="auto"
         />
       </div>
-      <div style={{ width: "70%" }}>
-        Luftfuktighet terskelverdi
+      <div style={{ width: "calc(100% - 40px)", padding:"5px 20px" }}>
+          <span style={{color: "#0090a8"}}>Luftfuktighet</span>
         <Slider
+            classes={
+                generateStyle(formVerdier.humidityMax, 100, 40, true)
+            }
+
           value={[formVerdier.humidityMin, formVerdier.humidityMax]}
           onChange={handleSliderEndring("humidity")}
           step={1}
@@ -209,12 +239,14 @@ function TerskelForm() {
               label: "100%",
             },
           ]}
-          valueLabelDisplay="off"
+          valueLabelDisplay="auto"
         />
       </div>
-      <div style={{ width: "70%" }}>
-        Tåke terskelverdi
+      <div style={{ width: "calc(100% - 40px)", padding:"5px 20px", backgroundColor: "#dff2f6" }}>
+          <span style={{color: "#0090a8"}}>Tåke</span>
         <Slider
+            disabled
+
           value={formVerdier.fog}
           onChange={handleSliderEndringSingleValue("fog")}
           step={1}
@@ -235,12 +267,16 @@ function TerskelForm() {
               label: "100%",
             },
           ]}
-          valueLabelDisplay="off"
+          valueLabelDisplay="auto"
         />
       </div>
-      <div style={{ width: "70%" }}>
-        Crosswind terskelverdi
+      <div style={{ width: "calc(100% - 40px)", padding:"5px 20px" }}>
+          <span style={{color: "#0090a8"}}>Crosswind</span>
         <Slider
+            classes={
+                generateStyle(formVerdier.crosswindMax, 60, 0, true)
+            }
+
           value={[formVerdier.crosswindMin, formVerdier.crosswindMax]}
           onChange={handleSliderEndring("crosswind")}
           step={1}
@@ -261,10 +297,12 @@ function TerskelForm() {
               label: "60 kt",
             },
           ]}
-          valueLabelDisplay="off"
+          valueLabelDisplay="auto"
         />
       </div>
     </form>
+        <Divider />
+        <Button onClick={tilbakestillTerskelverdier} variant="outlined">Tilbakestill</Button>
     </>
   )
 }
