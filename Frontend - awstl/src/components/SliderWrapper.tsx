485e0d9fb58055bfc4ac0a63ef0d8b2b0d3cdb33
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-09 21:03:30 +0100

    wrapper for slider to fix render problem, effective check makes values weird

---
+++ Frontend - awstl/src/components/SliderWrapper.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Slider } from "@mui/material";
+import { generateStyle } from '../util/sliderStyleUtil'
+
+
+function SliderWrapper(props: any) {
+
+    return (
+
+    <Slider
+        classes={
+            generateStyle(props.maxValue, props.max, props.min, props.reverse)
+        }
+        onChange={props.handleSliderEndring(props.field)}
+        value={[props.minValue, props.maxValue]}
+        step={props.step}
+        min={props.min}
+        max={props.max}
+
+        marks={props.marks}
+        valueLabelDisplay="auto"
+    />
+
+    )
+
+}
+
+function chechEqual(prev: any, next: any) {
+    const bool = (prev.minValue === next.minValue && prev.maxValue === next.maxValue)
+
+    if (!bool) {
+        //console.log("rerender", prev.field)
+        //console.log(prev.minValue, next.minValue)
+       // console.log(prev.field, next.field)
+        //console.log(prev, next)
+    }
+
+
+    return bool
+}
+
+//export default React.memo(SliderWrapper, chechEqual)
+export default React.memo(SliderWrapper)
