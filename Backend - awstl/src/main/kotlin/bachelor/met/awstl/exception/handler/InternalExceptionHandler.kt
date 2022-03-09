883c0c5b50ec7faaf41d3c1f94b5bfafe3716343
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-09 12:13:07 +0100

    new handler added for missing param

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/exception/handler/InternalExceptionHandler.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/exception/handler/InternalExceptionHandler.kt
@@ -7,6 +7,7 @@ import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import java.time.LocalDateTime
@@ -16,6 +17,18 @@ import java.time.LocalDateTime
 class InternalExceptionHandler {
 
     val logger: Logger = LoggerFactory.getLogger(InternalExceptionHandler::class.java)
+
+    @ExceptionHandler(value = [MissingServletRequestParameterException::class])
+    fun missingParamException(e: MissingServletRequestParameterException): ResponseEntity<Any> {
+
+        val response = InternalExceptionResponse(
+            LocalDateTime.now(),
+            e.message,
+            HttpStatus.BAD_REQUEST
+        )
+
+        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+    }
 
     @ExceptionHandler(value = [Exception::class])
     fun handleInternalException(e: Exception): ResponseEntity<Any> {
