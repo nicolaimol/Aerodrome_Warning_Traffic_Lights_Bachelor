bce2576486ff460cf2a8338ce60a1fc9dc259963
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-11 15:35:06 +0100

    skeleton for værbokser

--- Frontend - awstl/src/pages/Hjem.tsx
+++ Frontend - awstl/src/pages/Hjem.tsx
@@ -1,10 +1,14 @@
-import { Container } from '@mui/material'
+import {Box, Card, CardContent, CardHeader, Container, Skeleton, Typography } from '@mui/material'
 import React from 'react'
 import Banner from '../components/Banner'
 import Arbeider from '../components/Arbeider'
 import RaskVaer from '../components/RaskVaer'
+import { useSelector } from 'react-redux'
 
 function Hjem() {
+
+  const nowcast = useSelector((state: any) => state.nowcast.value)
+
   return (
     <>
     <Banner />
@@ -12,6 +16,194 @@ function Hjem() {
         <Arbeider />
     </Container>
     <RaskVaer />
+      {nowcast == undefined &&
+          <Container sx={{mb: 5}}>
+            <div style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr", gap: '3em'}}>
+              <div>
+                <Card sx={{ minWidth: 250, mb: 5, width: "100%", height: "fit-content" }}>
+                  <Typography variant="h1">
+                    <Skeleton variant="text" animation="wave"  />
+                  </Typography>
+                  <div style={{ display: 'flex', justifyContent: 'center'}}> {/** Div med hele kortet */}
+                    <CardContent>
+                      {/** Ikonet */}
+                      <Box style={{ display: 'flex', justifyContent: 'center'}}>
+                        <Skeleton variant="circular" animation="wave" sx={{margin: 'auto'}} width={80} height={80} />
+                      </Box>
+                      {/** Temperatur print */}
+                      <Box>
+                        <div style={{ display: 'flex', justifyContent: 'center'}}>
+                          <Typography sx={{fontSize: 38, width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                        </div>
+                      </Box>
+                      {/** Box med vindretning, vindfart og vindretning pil */}
+                      <Box style={{ display: 'flex', justifyContent: 'space-between',
+                        flexDirection: 'row', alignItems: 'center'}}>
+                        <div >
+                          <Skeleton variant="circular" animation="wave" height={40} width={40} />
+                        </div>
+                        <div style={{display: 'flex', flexDirection: 'column', color: '#0090a8'}}>
+                          <Typography style={{width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                          <Typography style={{width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                        </div>
+                      </Box>
+                    </CardContent>
+                  </div>
+
+                </Card>
+
+
+
+                <Skeleton variant="text" animation="wave"  />
+                <Skeleton variant="circular" animation="wave" sx={{margin: 'auto'}} width={80} height={80} />
+                <Skeleton variant="rectangular" animation="wave"  height={118} />
+              </div>
+              <div>
+                <Card sx={{ minWidth: 250, mb: 5, width: "100%", height: "fit-content" }}>
+                  <Typography variant="h1">
+                    <Skeleton variant="text" animation="wave"  />
+                  </Typography>
+                  <div style={{ display: 'flex', justifyContent: 'center'}}> {/** Div med hele kortet */}
+                    <CardContent>
+                      {/** Ikonet */}
+                      <Box style={{ display: 'flex', justifyContent: 'center'}}>
+                        <Skeleton variant="circular" animation="wave" sx={{margin: 'auto'}} width={80} height={80} />
+                      </Box>
+                      {/** Temperatur print */}
+                      <Box>
+                        <div style={{ display: 'flex', justifyContent: 'center'}}>
+                          <Typography sx={{fontSize: 38, width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                        </div>
+                      </Box>
+                      {/** Box med vindretning, vindfart og vindretning pil */}
+                      <Box style={{ display: 'flex', justifyContent: 'space-between',
+                        flexDirection: 'row', alignItems: 'center'}}>
+                        <div >
+                          <Skeleton variant="circular" animation="wave" height={40} width={40} />
+                        </div>
+                        <div style={{display: 'flex', flexDirection: 'column', color: '#0090a8'}}>
+                          <Typography style={{width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                          <Typography style={{width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                        </div>
+                      </Box>
+                    </CardContent>
+                  </div>
+
+                </Card>
+
+
+
+                <Skeleton variant="text" animation="wave"  />
+                <Skeleton variant="circular" animation="wave" sx={{margin: 'auto'}} width={80} height={80} />
+                <Skeleton variant="rectangular" animation="wave"  height={118} />
+              </div>
+              <div>
+                <Card sx={{ minWidth: 250, mb: 5, width: "100%", height: "fit-content" }}>
+                  <Typography variant="h1">
+                    <Skeleton variant="text" animation="wave"  />
+                  </Typography>
+                  <div style={{ display: 'flex', justifyContent: 'center'}}> {/** Div med hele kortet */}
+                    <CardContent>
+                      {/** Ikonet */}
+                      <Box style={{ display: 'flex', justifyContent: 'center'}}>
+                        <Skeleton variant="circular" animation="wave" sx={{margin: 'auto'}} width={80} height={80} />
+                      </Box>
+                      {/** Temperatur print */}
+                      <Box>
+                        <div style={{ display: 'flex', justifyContent: 'center'}}>
+                          <Typography sx={{fontSize: 38, width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                        </div>
+                      </Box>
+                      {/** Box med vindretning, vindfart og vindretning pil */}
+                      <Box style={{ display: 'flex', justifyContent: 'space-between',
+                        flexDirection: 'row', alignItems: 'center'}}>
+                        <div >
+                          <Skeleton variant="circular" animation="wave" height={40} width={40} />
+                        </div>
+                        <div style={{display: 'flex', flexDirection: 'column', color: '#0090a8'}}>
+                          <Typography style={{width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                          <Typography style={{width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                        </div>
+                      </Box>
+                    </CardContent>
+                  </div>
+
+                </Card>
+
+
+
+                <Skeleton variant="text" animation="wave"  />
+                <Skeleton variant="circular" animation="wave" sx={{margin: 'auto'}} width={80} height={80} />
+                <Skeleton variant="rectangular" animation="wave"  height={118} />
+              </div>
+              <div>
+                <Card sx={{ minWidth: 250, mb: 5, width: "100%", height: "fit-content" }}>
+                  <Typography variant="h1">
+                    <Skeleton variant="text" animation="wave"  />
+                  </Typography>
+                  <div style={{ display: 'flex', justifyContent: 'center'}}> {/** Div med hele kortet */}
+                    <CardContent>
+                      {/** Ikonet */}
+                      <Box style={{ display: 'flex', justifyContent: 'center'}}>
+                        <Skeleton variant="circular" animation="wave" sx={{margin: 'auto'}} width={80} height={80} />
+                      </Box>
+                      {/** Temperatur print */}
+                      <Box>
+                        <div style={{ display: 'flex', justifyContent: 'center'}}>
+                          <Typography sx={{fontSize: 38, width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                        </div>
+                      </Box>
+                      {/** Box med vindretning, vindfart og vindretning pil */}
+                      <Box style={{ display: 'flex', justifyContent: 'space-between',
+                        flexDirection: 'row', alignItems: 'center'}}>
+                        <div >
+                          <Skeleton variant="circular" animation="wave" height={40} width={40} />
+                        </div>
+                        <div style={{display: 'flex', flexDirection: 'column', color: '#0090a8'}}>
+                          <Typography style={{width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                          <Typography style={{width: '2em'}}>
+                            <Skeleton variant="text" animation="wave"  />
+                          </Typography>
+                        </div>
+                      </Box>
+                    </CardContent>
+                  </div>
+
+                </Card>
+
+
+
+                <Skeleton variant="text" animation="wave"  />
+                <Skeleton variant="circular" animation="wave" sx={{margin: 'auto'}} width={80} height={80} />
+                <Skeleton variant="rectangular" animation="wave"  height={118} />
+              </div>
+
+            </div>
+
+          </Container>
+      }
     </>
   )
 }
