a17505c9c22ed643cfe056a141508332249cc8e5
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-11 15:37:55 +0100

    added returntype and status for swagger UI

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/exception/handler/TerskelverdiNotFoundExceptionHandler.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/exception/handler/TerskelverdiNotFoundExceptionHandler.kt
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders.SET_COOKIE
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseCookie
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import java.time.Duration
 
 @ControllerAdvice
@@ -17,6 +19,7 @@ class TerskelverdiNotFoundExceptionHandler {
 
     val logger = LoggerFactory.getLogger(TerskelverdiNotFoundExceptionHandler::class.java)
 
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = [TerskelverdiNotFoundException::class])
     fun terskelverdiNotFoundExceptionHandler(e: TerskelverdiNotFoundException): ResponseEntity<Any> {
 
