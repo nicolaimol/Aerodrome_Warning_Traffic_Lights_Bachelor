1037e96051e9d08612c97623c65ccb49cdacba31
Erik Myhre <myherik@gmail.com>
2022-03-11 15:30:30 +0100

    calcFarge funksjon

---
+++ Frontend - awstl/src/util/calcFarge.ts
@@ -0,0 +1,23 @@
+
+export const calcFarge = ( data?: any, terskelverdier?: any ) => {
+
+    console.log('Her er terskelverdier fra calcFarge');
+    console.log(terskelverdier!!);
+
+    console.log('Her er data fra calcFarge');
+    console.log(data!!);
+
+    let analyseArray = [];
+
+    // Sjekk temperatur
+    if (data?.air_temperature > terskelverdier?.airTempMin && data?.air_temperature < terskelverdier?.airTempMax) {
+        analyseArray.push('yellow');
+    } else if (data?.air_temperature < terskelverdier?.airTempMin) {
+        return 'red';
+    }
+
+    if (analyseArray.length > 0){
+        return 'yellow';
+    }
+    return 'green';
+}
