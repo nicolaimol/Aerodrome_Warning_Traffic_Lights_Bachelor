10d8575df9c0d7491c7ab730f2b242450f2be1e8
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-23 09:56:46 +0100

    start of provider, some error in constructor usage KeycloakBuilder

---
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/config/KeycloakProvider.kt
@@ -0,0 +1,41 @@
+package bachelor.met.awstl.config
+
+import org.keycloak.OAuth2Constants
+import org.keycloak.admin.client.Keycloak
+import org.keycloak.admin.client.KeycloakBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class KeycloakProvider() {
+
+    @Value("\${keycloak.auth-server-url}")
+    var serverURL: String? = null
+
+    @Value("\${keycloak.realm}")
+    var realm: String? = null
+
+    @Value("\${keycloak.resource}")
+    var clientID: String? = null
+
+    @Value("\${keycloak.credentials.secret}")
+    var clientSecret: String? = null
+
+    private var keycloak: Keycloak? = null
+
+    fun getInstance(): Keycloak {
+        if (keycloak == null) {
+            keycloak = KeycloakBuilder()
+                .realm(realm!!)
+                .serverUrl(serverURL!!)
+                .clientId(clientID!!)
+                .clientSecret(clientSecret!!)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .build()
+        }
+
+        return keycloak!!
+    }
+
+
+}
