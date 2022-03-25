fbebf5256a2e09048a7216ca84f9b3e0ec39405a
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-25 09:31:02 +0100

    new keycloak with new username and password, and set in system

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/config/KeycloakProvider.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/config/KeycloakProvider.kt
@@ -1,10 +1,14 @@
 package bachelor.met.awstl.config
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder
 import org.keycloak.OAuth2Constants
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
 import org.keycloak.admin.client.Keycloak
 import org.keycloak.admin.client.KeycloakBuilder
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConfiguration.WithPassword
 
 @Configuration
 class KeycloakProvider() {
@@ -21,9 +25,12 @@ class KeycloakProvider() {
     @Value("\${keycloak.credentials.secret}")
     var clientSecret: String? = null
 
+    //private var keycloak: Keycloak? = null
     private var keycloak: Keycloak? = null
 
     fun getInstance(): Keycloak {
+
+        print("$serverURL $realm $clientID $clientSecret")
         if (keycloak == null) {
             keycloak = KeycloakBuilder
                 .builder()
@@ -31,12 +38,35 @@ class KeycloakProvider() {
                 .serverUrl(serverURL!!)
                 .clientId(clientID!!)
                 .clientSecret(clientSecret!!)
-                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .grantType(OAuth2Constants.PASSWORD)
                 .build()
         }
 
         return keycloak!!
     }
 
+    fun newKeycloakBuilderWithPasswordCredentials(username: String, password: String): Keycloak {
+        return KeycloakBuilder.builder()
+            .realm(realm!!)
+            .serverUrl(serverURL!!)
+            .clientId(clientID!!)
+            .clientSecret(clientSecret!!)
+            .grantType(OAuth2Constants.PASSWORD)
+            .username(username)
+            .password(password)
+            .build()
+    }
+
+    fun newKeycloakBuilderWithPasswordCredentials(): Keycloak {
+        return KeycloakBuilder.builder()
+            .realm(realm!!)
+            .serverUrl(serverURL!!)
+            .clientId(clientID!!)
+            .clientSecret(clientSecret!!)
+            .grantType(OAuth2Constants.PASSWORD)
+            .username("admin")
+            .password("admin")
+            .build()
+    }
 
 }
