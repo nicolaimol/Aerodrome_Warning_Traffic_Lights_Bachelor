e92a17c11c745108a3b6efaf6799cbb0aeca12b7
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-25 09:33:19 +0100

    added granted authorities to config

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/config/SecurityConfig.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/config/SecurityConfig.kt
@@ -1,22 +1,31 @@
 package bachelor.met.awstl.config
 
+import org.keycloak.adapters.KeycloakConfigResolver
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistry
 import org.springframework.security.core.session.SessionRegistryImpl
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 
 @KeycloakConfiguration
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 class SecurityConfig: KeycloakWebSecurityConfigurerAdapter() {
 
     @Autowired
     fun configureGlobal(auth: AuthenticationManagerBuilder) {
-        auth.authenticationProvider(keycloakAuthenticationProvider())
+        val keycloakAuthenticationProvider = keycloakAuthenticationProvider()
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+        auth.authenticationProvider(keycloakAuthenticationProvider)
     }
 
     @Bean
@@ -28,6 +37,14 @@ class SecurityConfig: KeycloakWebSecurityConfigurerAdapter() {
     protected fun buildSessionRegistry(): SessionRegistry {
         return SessionRegistryImpl()
     }
+
+    /*
+    @Bean
+    fun KeycloakConfigResolver(): KeycloakConfigResolver {
+        return KeycloakSpringBootConfigResolver()
+    }
+     */
+
 
     override fun configure(http: HttpSecurity?) {
         super.configure(http)
