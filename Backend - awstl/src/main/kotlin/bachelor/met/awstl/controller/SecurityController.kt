33fdf30871c6dbcd51c8fb35afbf6b05a2522fa3
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-27 11:29:53 +0200

    NEw endpoint for automatic authentication

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/controller/SecurityController.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/controller/SecurityController.kt
@@ -3,14 +3,27 @@ package bachelor.met.awstl.controller
 import bachelor.met.awstl.dto.LoginDto
 import bachelor.met.awstl.dto.RegisterDto
 import bachelor.met.awstl.service.KeycloakAdminClientService
+import org.apache.http.HttpResponse
+import org.keycloak.KeycloakSecurityContext
+import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount
 import org.keycloak.representations.AccessTokenResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextImpl
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.toMono
+import javax.annotation.security.RolesAllowed
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpSession
 import javax.validation.constraints.NotNull
 import javax.ws.rs.core.Response
 
@@ -18,11 +31,13 @@ import javax.ws.rs.core.Response
 @RestController
 class SecurityController(val service: KeycloakAdminClientService) {
 
-    @PostMapping(value = ["/login"])
-    fun login(@NotNull @RequestBody dto: LoginDto): ResponseEntity<AccessTokenResponse> {
-        val response = service.login(dto)
+    val logger: Logger = LoggerFactory.getLogger(SecurityController::class.java)
 
-        return ResponseEntity.ok(response)
+    @PostMapping(value = ["/login"])
+    fun login(@NotNull @RequestBody dto: LoginDto, response: HttpServletResponse): ResponseEntity<AccessTokenResponse> {
+        val responseValue = service.login(dto, response)
+
+        return ResponseEntity.ok(responseValue)
     }
 
     @PostMapping(value = ["/create"])
@@ -31,6 +46,32 @@ class SecurityController(val service: KeycloakAdminClientService) {
 
         return ResponseEntity.status(response.status).build()
 
+    }
+
+    @GetMapping(value = ["/auth"])
+    fun authUser(): ResponseEntity<String> {
+
+        val map = mapOf(Pair("message", "ok"))
+
+        val sessionObj = session!!.getAttribute("SPRING_SECURITY_CONTEXT") as SecurityContextImpl
+        sessionObj.authentication
+        logger.info("Controller id: ${session!!.id}")
+        logger.info(session.toString())
+
+        return ResponseEntity.ok(sessionObj.toString())
+    }
+
+    @Autowired
+    val session: HttpSession? = null
+
+    @GetMapping(value = ["/test"])
+    //@RolesAllowed("admin")
+    fun test() {
+        logger.info("tester session variabler")
+
+        val securityContext = session!!.getAttribute("SPRING_SECURITY_CONTEXT")
+        logger.info(session!!.id)
+        logger.info( securityContext?.toString())
     }
 
     @GetMapping(value = ["/{username}"])
