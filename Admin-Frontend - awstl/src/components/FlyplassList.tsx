29fda457c6f61abbca776a05583a4be0e736f249
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-29 21:52:39 +0200

    soon to be flyplass listing

---
+++ Admin-Frontend - awstl/src/components/FlyplassList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {TableContainer} from "@mui/material";
+
+function FlyplassList(props: any) {
+    return (
+        <div>
+            {/*
+            <TableContainer>
+                <Table>
+
+                </Table>
+            </TableContainer>
+            */}
+        </div>
+    );
+}
+
+export default FlyplassList;
