5b36efd026dbb117880d09e13a018935527ef12c
s341833 <56063595+s341833@users.noreply.github.com>
2022-03-29 21:52:04 +0200

    routing for home, flyplass and terskelverdi

---
+++ Admin-Frontend - awstl/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
+import './App.css';
+import Hjem from './pages/Hjem'
+import Flyplass from './pages/Flyplass'
+import Terskel from './pages/Terskel'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div style={{display: 'flex',flexDirection:'column', minHeight: '100vh'}}>
+        <Navbar />
+      <div style={{display: 'flex', flexGrow: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red'}}>
+        <Routes>
+          <Route path="/flyplass" element={<Flyplass />} />
+          <Route path="/terskelverdi" element={<Terskel />} />
+          <Route path="/" element={<Hjem />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
