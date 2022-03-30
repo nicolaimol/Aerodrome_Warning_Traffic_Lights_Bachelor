a272e21b3922858f5bbf4c58b9ef27816a25e715
Erik Myhre <myherik@gmail.com>
2022-03-30 11:16:58 +0200

    farger til avg dep

--- Frontend - awstl/src/components/PilotVelgDepArvl.tsx
+++ Frontend - awstl/src/components/PilotVelgDepArvl.tsx
@@ -1,21 +1,49 @@
 import { TextField, Typography } from '@mui/material'
 import React from 'react'
+import { ThemeProvider, styled } from '@mui/material/styles';
+import { theme } from '../util/theme'
+
+
+const CustomTextFieldTimeInput = styled(TextField)({
+  input:{
+    color: '#0090a8',
+  },
+  label:{
+    color: '#0090a8',
+  },
+  '& label.Mui-focused': {
+    color: '#0090a8',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#0090a8',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#0090a8',
+    },
+    '&:hover fieldset': {
+      borderColor: '#496c80',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#0090a8',
+    },
+  },
+});
 
 
 
 function PilotVelgDepArvl() {
 
-
   return (
     <>
-    <div style={{ display: 'flex', justifyContent: 'center', flexFlow: 'column wrap', textAlign: 'center'}}>
+    <ThemeProvider theme={theme}>
+      <div style={{ display: 'flex', justifyContent: 'center', flexFlow: 'column wrap', textAlign: 'center'}}>
         <Typography sx={{ mb: 3 }} variant='h5' color='primary'>
                 Velg tidspunkt for avgang og ankomst
             </Typography>
             <div style={{ display: 'flex', justifyContent: 'space-evenly', flexFlow: 'row wrap'}}>
-                <TextField
+                <CustomTextFieldTimeInput
                 id="time"
-                color= 'secondary'
                 label="Avgang"
                 type="time"
                 defaultValue="07:30"
@@ -27,21 +55,24 @@ function PilotVelgDepArvl() {
                 }}
                 sx={{ width: 150, mb: 5 }}
             />
-            <TextField
-                id="time"
-                label="Ankomst"
-                type="time"
-                defaultValue="07:30"
-                InputLabelProps={{
-                shrink: true,
-                }}
-                inputProps={{
-                step: 300, // 5 min
-                }}
-                sx={{ width: 150, mb: 5 }}
+            <CustomTextFieldTimeInput 
+            id="time"
+            label="Ankomst"
+            type="time"
+            defaultValue="07:30"
+            InputLabelProps={{
+            shrink: true,
+            }}
+            inputProps={{
+            step: 300, // 5 min
+            }}
+            sx={{ width: 150, mb: 5 }}
             />
+
             </div>
     </div>
+    </ThemeProvider>
+    
     
     
     </>
