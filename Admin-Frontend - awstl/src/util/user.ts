7bb729796df08ac5141acb487ebdc602e4f57281
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-01 09:18:23 +0200

    new login functionality

--- Admin-Frontend - awstl/src/util/user.ts
+++ Admin-Frontend - awstl/src/util/user.ts
@@ -1,19 +1,25 @@
 import axios from 'axios'
+import {useContext} from "react";
+import {TokenContext} from "./DataContext";
 
-export const login = async (username: string, password: string): Promise<boolean> => {
+export const login = async (username: string, password: string): Promise<string> => {
     const loginDto = {
         username: username,
         password: password
     }
 
+    //const {token, setToken} = useContext(TokenContext)
+
     console.log(loginDto)
 
     let loggedIn = await axios.post("/api/user/login", loginDto)
         .then((response: any) => {
-            return true;
+            console.log(response)
+
+            return response.data.access_token;
         })
         .catch((error: any) => {
-            return false;
+            return null;
         })
 
     return loggedIn
