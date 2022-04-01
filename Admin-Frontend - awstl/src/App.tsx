881a8ca8d467947901c7d2e3d34d8c64aeee7ea0
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-01 09:16:15 +0200

    wrapped to use token context in all child-element

--- Admin-Frontend - awstl/src/App.tsx
+++ Admin-Frontend - awstl/src/App.tsx
@@ -5,19 +5,22 @@ import Hjem from './pages/Hjem'
 import Flyplass from './pages/Flyplass'
 import Terskel from './pages/Terskel'
 import Navbar from './components/Navbar'
+import { TokenContextProvider} from "./util/DataContext";
 
 function App() {
   return (
-    <div style={{display: 'flex',flexDirection:'column', minHeight: '100vh'}}>
-        <Navbar />
-      <div style={{display: 'flex', flexGrow: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red'}}>
-        <Routes>
-          <Route path="/flyplass" element={<Flyplass />} />
-          <Route path="/terskelverdi" element={<Terskel />} />
-          <Route path="/" element={<Hjem />} />
-        </Routes>
-      </div>
-    </div>
+      <TokenContextProvider>
+          <div style={{display: 'flex',flexDirection:'column', minHeight: '100vh'}}>
+              <Navbar />
+              <div style={{display: 'flex', flexGrow: 1, alignItems: 'center', justifyContent: 'center',}}>
+                  <Routes>
+                      <Route path="/flyplass" element={<Flyplass />} />
+                      <Route path="/terskelverdi" element={<Terskel />} />
+                      <Route path="/" element={<Hjem />} />
+                  </Routes>
+              </div>
+          </div>
+      </TokenContextProvider>
   );
 }
 
