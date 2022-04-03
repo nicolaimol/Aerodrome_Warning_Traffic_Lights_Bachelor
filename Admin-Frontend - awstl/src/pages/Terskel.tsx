cd362c90d04b11926c8500a04e66787bd015da98
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-03 14:19:10 +0200

    added new list with authentcation

--- Admin-Frontend - awstl/src/pages/Terskel.tsx
+++ Admin-Frontend - awstl/src/pages/Terskel.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useKeycloak} from "@react-keycloak/web";
+import {hentTerskel} from "../util/terskel";
+import TerskelList from "../components/TerskelList";
 
 function Terskel(props: any) {
+
+    const keycloak = useKeycloak()
+
+    const [terskelList, setTerskelList] = useState([])
+
+    useEffect(() => {
+
+        const hent = async () => {
+            const data = await hentTerskel(keycloak.keycloak.token!!)
+            if (data != null) {
+                setTerskelList(data)
+            }
+        }
+
+
+
+    })
+
     return (
         <div>
-            TERSKEL
+            <TerskelList list={terskelList} />
         </div>
     );
 }
