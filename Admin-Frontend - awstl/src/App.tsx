2efcf155d3cbabf1f34f072fbe2bbb649d8ffce7
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-03 13:38:57 +0200

    Admin uses react keycloak client to authenticate to awtl realm

--- Admin-Frontend - awstl/src/App.tsx
+++ Admin-Frontend - awstl/src/App.tsx
@@ -5,18 +5,27 @@ import Hjem from './pages/Hjem'
 import Flyplass from './pages/Flyplass'
 import Terskel from './pages/Terskel'
 import Navbar from './components/Navbar'
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./util/keycloak"
+import SecureRoute from "./components/SecureRoute";
 
 function App() {
   return (
           <div style={{display: 'flex',flexDirection:'column', minHeight: '100vh'}}>
-              <Navbar />
-              <div style={{display: 'flex', flexGrow: 1, alignItems: 'center', justifyContent: 'center',}}>
-                  <Routes>
-                      <Route path="/flyplass" element={<Flyplass />} />
-                      <Route path="/terskelverdi" element={<Terskel />} />
-                      <Route path="/" element={<Hjem />} />
-                  </Routes>
-              </div>
+              <ReactKeycloakProvider authClient={keycloak}>
+                  <Navbar />
+                  <div style={{display: 'flex', flexGrow: 1, alignItems: 'center', justifyContent: 'center',}}>
+                      <Routes>
+                          <Route path="/flyplass" element={
+                              <SecureRoute role="user">
+                                  <Flyplass />
+                              </SecureRoute>
+                          } />
+                          <Route path="/terskelverdi" element={<Terskel />} />
+                          <Route path="/" element={<Hjem />} />
+                      </Routes>
+                  </div>
+              </ReactKeycloakProvider>
           </div>
   );
 }
