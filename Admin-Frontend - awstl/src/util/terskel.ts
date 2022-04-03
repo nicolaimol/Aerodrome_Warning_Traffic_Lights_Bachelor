3391a5e95996336ade205d5abb4a17b70be030b0
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-03 15:03:14 +0200

    new function for deleting terskel

--- Admin-Frontend - awstl/src/util/terskel.ts
+++ Admin-Frontend - awstl/src/util/terskel.ts
@@ -7,9 +7,9 @@ export const hentTerskel = async (token: string) => {
         }
     }
 
-    let response = await axios.put("/api/terskel/all", config)
+    const response = await axios.get("/api/terskel/all", config)
         .then((response: any) => {
-            console.log(response.data)
+            //console.log(response.data)
             return response.data
         })
         .catch((error: any) => {
@@ -20,4 +20,27 @@ export const hentTerskel = async (token: string) => {
     return response
 
 
+}
+
+export const slettTerskel = async (id: string, token: string) => {
+    let config = {
+        headers: {
+            "Authorization": `Bearer ${token}`,
+        },
+        data: {
+            id: id
+        }
+    }
+
+    const response = await axios.delete("/api/terskel/", config)
+        .then((response: any) => {
+            console.log(response.status)
+            return response.status
+        })
+        .catch((error: any) => {
+            console.log(error.response.status)
+            return error.response.status
+        })
+
+    return response
 }
