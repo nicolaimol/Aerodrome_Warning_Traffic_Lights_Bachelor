8a9c757f21c314f441a63670dcb8b76c60ac60ea
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-04 10:57:22 +0200

    added delete and add endpoint

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/controller/FlyplassController.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/controller/FlyplassController.kt
@@ -1,12 +1,15 @@
 package bachelor.met.awstl.controller
 
 import bachelor.met.awstl.dto.FlyplassDto
+import bachelor.met.awstl.dto.FlyplassSlettDto
 import bachelor.met.awstl.mapper.FlyplassToFlyplassDto
 import bachelor.met.awstl.model.Flyplass
 import bachelor.met.awstl.service.FlyplassService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -45,6 +48,24 @@ class FlyplassController(var service: FlyplassService) {
         service.updateFlyplass(flyplass.icao, flyplass)
 
         return ResponseEntity.ok().build()
+    }
+
+    @PostMapping(value = ["/airport"])
+    @RolesAllowed(value = ["admin", "user"])
+    fun addAirport(@RequestBody flyplass: Flyplass): ResponseEntity<Any> {
+        service.addFlyplass(flyplass)
+
+        return ResponseEntity.ok().build()
+    }
+
+    @DeleteMapping(value = ["/airport"])
+    @RolesAllowed(value = ["admin", "user"])
+    fun deleteAirport(@RequestBody dto: FlyplassSlettDto): ResponseEntity<Any> {
+
+        service.deleteFlyplass(dto.icao)
+
+        return ResponseEntity.ok().build()
+
     }
 
 }
