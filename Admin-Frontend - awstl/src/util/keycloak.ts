47bc5269bcce721224f8488b68fe1d113bb332d8
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-08 19:28:48 +0200

    keycloak congig uses env

--- Admin-Frontend - awstl/src/util/keycloak.ts
+++ Admin-Frontend - awstl/src/util/keycloak.ts
@@ -3,8 +3,8 @@ import Keycloak from 'keycloak-js'
 // @ts-ignore
 const keycloak = new Keycloak({
     url: "/keycloak/auth",
-    realm: 'awtl',
-    clientId: 'test'
+    realm: process.env.REACT_APP_KEYCLOAK_REALM || "awtl",
+    clientId: process.env.REACT_APP_KEYCLOAK_CLIENT || "awtl-admin",
 })
 
 export default keycloak;
