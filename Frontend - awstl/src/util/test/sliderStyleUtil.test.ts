36d419fc1b315667a17881edbc63f69dcf3ff601
Erik Myhre <myherik@gmail.com>
2022-04-12 22:16:37 +0200

    test for slider styles

---
+++ Frontend - awstl/src/util/test/sliderStyleUtil.test.ts
@@ -0,0 +1,8 @@
+//import { sliderStyleUtil } from '../sliderStyleUtil';
+
+test('sliderStyle test', () => {
+
+    // Hvis du skal lage en expect så må du sette disse som props
+    // expect(sliderStyleUtil(value: number, max: number, min: number, revers: boolean, disabled: boolean))
+    //expect(sliderStyleUtil(50, 75, 25, false, false)).toBe();
+})
