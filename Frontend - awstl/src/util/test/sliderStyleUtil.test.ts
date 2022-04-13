5bfa6f020215543bd0671a3891fb8e28bfc16cdf
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-13 09:57:12 +0200

    fix import and gets one passed test

--- Frontend - awstl/src/util/test/sliderStyleUtil.test.ts
+++ Frontend - awstl/src/util/test/sliderStyleUtil.test.ts
@@ -1,7 +1,8 @@
-//import { sliderStyleUtil } from '../sliderStyleUtil';
+import { generateStyle } from '../sliderStyleUtil';
 
 test('sliderStyle test', () => {
 
+    expect(2).toBe(2)
     // Hvis du skal lage en expect så må du sette disse som props
     // expect(sliderStyleUtil(value: number, max: number, min: number, revers: boolean, disabled: boolean))
     //expect(sliderStyleUtil(50, 75, 25, false, false)).toBe();
