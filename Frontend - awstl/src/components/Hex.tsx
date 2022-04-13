d350c83fec1e7f1a174aebeb9aaf125834c10b3c
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-13 09:51:30 +0200

    new placement of ! based on color, also ! color based on color

--- Frontend - awstl/src/components/Hex.tsx
+++ Frontend - awstl/src/components/Hex.tsx
@@ -8,13 +8,16 @@ function Hex(props: any) {
             backgroundPosition: 'center',
             width: '25px',
             height: '25px',
-            color: 'black',
+            color: props.color === 'red' ? 'white' : 'black',
             fontSize: '20px',
             display: 'flex',
             textAlign: 'center',
             alignItems: 'center',
             justifyContent: 'center',
-            marginLeft: '0.5em'}}><span style={{transform: 'translateX(-6%)'}}>!</span></span>
+            marginLeft: '0.5em'}}><span style={{
+                    transform: props.color === 'red'
+                        ? 'translateX(-6%) translateY(5%)'
+                        : 'translateX(-6%) translateY(15%)'}}>!</span></span>
     );
 }
 
