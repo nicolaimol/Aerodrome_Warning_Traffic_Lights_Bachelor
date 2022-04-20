411dc2219c388b6c10fad1893694d740dbb771ac
Erik Myhre <myherik@gmail.com>
2022-04-20 10:38:11 +0200

    dårlig test :(

--- Frontend - awstl/src/util/test/sliderStyleUtil.test.ts
+++ Frontend - awstl/src/util/test/sliderStyleUtil.test.ts
@@ -1,9 +1,32 @@
 import { generateStyle } from '../sliderStyleUtil';
 
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import SliderWrapper from "../../components/SliderWrapper";
+
+
 test('sliderStyle test', () => {
 
-    expect(2).toBe(2)
     // Hvis du skal lage en expect så må du sette disse som props
     // expect(sliderStyleUtil(value: number, max: number, min: number, revers: boolean, disabled: boolean))
-    //expect(sliderStyleUtil(50, 75, 25, false, false)).toBe();
+
+    expect(generateStyle(50, 75, 25, false, true)).toEqual({
+        mark: "makeStyles-mark-2",
+        markLabel: "makeStyles-markLabel-3",
+        rail: "makeStyles-rail-6",
+        thumb: "makeStyles-thumb-1",
+        track: "makeStyles-track-4",
+        valueLabel: "makeStyles-valueLabel-5"
+    })
+
+    
 })
+
+test('slider diabled', () => {
+
+    expect(generateStyle(50, 75, 25, false, false)).toEqual({})
+
+})
+
+
