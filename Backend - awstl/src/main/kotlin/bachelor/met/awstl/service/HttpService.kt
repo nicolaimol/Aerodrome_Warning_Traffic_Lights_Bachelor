3dcc8e7e148df11417d69926a934e7d002fa33f1
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-20 10:40:55 +0200

    overload with cache type to store expire time

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/service/HttpService.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/service/HttpService.kt
@@ -1,7 +1,9 @@
 package bachelor.met.awstl.service
 
+import bachelor.met.awstl.enum.Cache
 import bachelor.met.awstl.exception.TafMetarNotAvailableException
 import bachelor.met.awstl.exception.UnknownHostException
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import java.util.HashMap
@@ -9,13 +11,32 @@ import org.springframework.web.client.ResourceAccessException
 import kotlin.reflect.typeOf
 
 @Service
-class HttpService(private val template: RestTemplate) {
+class HttpService(private val template: RestTemplate, private val cacheService: CacheService) {
 
     fun <T> hentData(url: String, clazz: Class<T>, query: HashMap<String, String>): T {
         try {
             val result: T? = template.getForObject(url, clazz, query)
 
             return result!!
+        } catch (e: ResourceAccessException) {
+            throw UnknownHostException(e.message)
+        } catch (e: NullPointerException) {
+            when (clazz) {
+                String::class.java -> throwTafMetarException()
+            }
+
+            throw NullPointerException(e.message)
+        }
+    }
+
+    fun <T> hentData(url: String, clazz: Class<T>, query: HashMap<String, String>, icao: String, type: Cache): T {
+        try {
+            val result: ResponseEntity<T> = template.getForEntity(url, clazz, query)
+
+            cacheService.setTimeInCache(result.headers["expires"]?.get(0)!!, icao, type)
+
+
+            return result.body!!
         } catch (e: ResourceAccessException) {
             throw UnknownHostException(e.message)
         } catch (e: NullPointerException) {
