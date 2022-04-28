568ab1a81d58305bc42c808358c57e8ef2143a26
Erik Myhre <myherik@gmail.com>
2022-04-28 17:40:15 +0200

    Nå begynner footer å ligne på noe her

--- Frontend - awstl/src/components/Footer.tsx
+++ Frontend - awstl/src/components/Footer.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import { BottomNavigation, BottomNavigationAction, Box, Typography } from '@mui/material'
+import { Box, Container } from '@mui/material'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Footer() {
   return (
-    <Box>
-      <BottomNavigation sx={{ backgroundColor: '#496c80', height: 200 }}>
-        {/*
-        <Typography
-        variant= 'h6'
-        sx={{ color: 'white'}}>
-            Dette er en Footer
-        </Typography>
-        */}
-      </BottomNavigation>
+    <Box sx={{ backgroundColor: '#496c80', minHeight: 200, width: '100%' }}>
+      <Container>
+        <div style={{ display: 'flex', justifyContent: 'space-between', textAlign: 'left', flexWrap: 'wrap', color: '#FFFFFF'}}>
+          <div style={{ display: 'flex', justifyContent: 'center', width: '33%', minWidth: '200px'}}>
+            <p>Meteorologisk Institutt<br/>Henrik Mohns Plass 1<br/>0371 Oslo<br/>Telefon 22 96 30 00</p>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', width: '33%', minWidth: '200px'}}>
+            <p><u><a href="#">Veibeskrivelse</a></u><br/><u><a href="#">Kontakt oss</a></u><br/><u><a href="#">Personvern</a></u></p>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', width: '33%', minWidth: '200px', paddingTop: '1em', marginBottom: '1em'}}>
+          <a href="#"><FacebookIcon sx={{ fontSize: 50 }}></FacebookIcon></a>
+          <a href="#"><TwitterIcon sx={{ fontSize: 50 }}></TwitterIcon></a>
+          <a href="#"><InstagramIcon sx={{ fontSize: 50 }}></InstagramIcon></a>
+            
+            
+            
+          </div>
+        </div>
+      </Container>
+
     </Box>
   )
 }
