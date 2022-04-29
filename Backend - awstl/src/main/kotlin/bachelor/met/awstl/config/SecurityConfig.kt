3254e5e308ecbb908c002920aa66ba6814bf23da
s341833 <56063595+s341833@users.noreply.github.com>
2022-04-29 10:05:04 +0200

    removed testing endpoint

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/config/SecurityConfig.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/config/SecurityConfig.kt
@@ -58,7 +58,6 @@ class SecurityConfig: KeycloakWebSecurityConfigurerAdapter() {
         http!!
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/api/user/auth").authenticated()
             .anyRequest().permitAll()
     }
 
