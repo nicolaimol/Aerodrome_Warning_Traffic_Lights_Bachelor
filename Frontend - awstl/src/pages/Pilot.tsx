be9a0924ad2f704096947f5ac28455f9b39cf145
Erik Myhre <myherik@gmail.com>
2022-05-09 12:44:56 +0200

    Rydding i Pilot

--- Frontend - awstl/src/pages/Pilot.tsx
+++ Frontend - awstl/src/pages/Pilot.tsx
@@ -21,23 +21,19 @@ function Pilot() {
   const airport = useSelector((state: any) => state.airport.value)
   const fromWeather = useSelector((state: any) => state.weather.value)
   const toAirportRedux = useSelector((state: any) => state.toAirport.value)
-  const [toAirport, setToAirport] = useState<any>(null)
-
-  const [nesteDag, setNesteDag] = useState<boolean>(false);
-
-  const [weatherToAirport, setWeatherToAirport] = useState<any>(undefined);
-
-    const updateAirportTo = (data: any) => {
-
-      setToAirport(data)
-    }
-
   const dateNow = new Date().toLocaleString();
-
   const tidNow = dateNow.split(' ')[1].split(':')[0] + ':' + dateNow.split(' ')[1].split(':')[1]
 
   let minutt:string = tidNow.split(':')[1];
   let tilTime:string = tidNow.split(':')[0];
+  
+  const [toAirport, setToAirport] = useState<any>(null)
+  const [nesteDag, setNesteDag] = useState<boolean>(false);
+  const [weatherToAirport, setWeatherToAirport] = useState<any>(undefined);
+  
+  const updateAirportTo = (data: any) => {
+    setToAirport(data)
+  }
 
   if (+minutt <= 30){
     minutt = '30';
@@ -58,41 +54,33 @@ function Pilot() {
 
   const printTid = tilTime + ':' + minutt;
 
-    const [avgangstid, setAvgangstid] = useState<any>(printTid);
-    const [ankomsttid, setAnkomsttid] = useState<any>(printAnkomstTid);
+  const [avgangstid, setAvgangstid] = useState<any>(printTid);
+  const [ankomsttid, setAnkomsttid] = useState<any>(printAnkomstTid);
+    
 
     useEffect(() => {
       if (toAirport != null) {
         const urlArvl = `/api/locationforecast?icao=${toAirport.icao}`
+        axios.get(urlArvl)
+          .then((response: any) => {
+          response.data.properties.timeseries.map((data: any) => {
+            // calculate effective air temperature and set it to the instant object
+            data.data.instant.details.effective_temperature = 
+              (13.12 + (data.data.instant.details.air_temperature * 0.6215)
+              - ((11.37 * Math.pow((data.data.instant.details.wind_speed * 3.6), 0.16)))
+              + ((data.data.instant.details.air_temperature * 0.3965 ) * (Math.pow((data.data.instant.details.wind_speed * 3.6), 0.16)))).toPrecision(2)
 
-      axios.get(urlArvl)
-            .then((response: any) => {
-              response.data.properties.timeseries.map((data: any) => {
-
-                // calculate effective air temperature and set it to the instant object
-                //if (data.properties.timeseries[0].data.instant.details.air_temperature < 10 && data.properties.timeseries[0].data.instant.details.wind_speed > 1.33) {
-                        data.data.instant.details.effective_temperature = 
-                        (13.12 + (data.data.instant.details.air_temperature * 0.6215)
-                        - ((11.37 * Math.pow((data.data.instant.details.wind_speed * 3.6), 0.16)))
-                        + ((data.data.instant.details.air_temperature * 0.3965 ) * (Math.pow((data.data.instant.details.wind_speed * 3.6), 0.16)))).toPrecision(2)
-                     
-                    
-                //}
-
-
-                data.data.instant.details.wind_speed = (data.data.instant.details.wind_speed * 1.943844).toPrecision(2);
-                data.data.instant.details.wind_speed_of_gust = (data.data.instant.details.wind_speed_of_gust * 1.943844).toPrecision(2);
-            })
-              setWeatherToAirport(response.data)
-            })
-            .catch((error:any) => {
-                if (error.status === 400) {
-                  setWeatherToAirport(null)
-                }
-            })
+            data.data.instant.details.wind_speed = (data.data.instant.details.wind_speed * 1.943844).toPrecision(2);
+            data.data.instant.details.wind_speed_of_gust = (data.data.instant.details.wind_speed_of_gust * 1.943844).toPrecision(2);
+          })
+          setWeatherToAirport(response.data)
+        })
+        .catch((error:any) => {
+          if (error.status === 400) {
+            setWeatherToAirport(null)
+          }
+        })
       }
-      
-      
     }, [toAirport])
 
     useEffect(() => {
@@ -108,7 +96,6 @@ function Pilot() {
       
     }, [toAirportRedux])
 
-  
     useEffect(() => {
       const dateNowBool = new Date().toLocaleString();
 
@@ -134,54 +121,45 @@ function Pilot() {
   return (
     <>
     <Container>
-    <div style={{ display: 'flex', justifyContent: 'space-evenly', textAlign: 'center', flexWrap: 'wrap', alignItems: 'center'}}>
-    </div>
+      <Typography sx={{ color: '#0090a8', fontSize: 40, textAlign: 'center', mt: 5}}>
+          Rute
+      </Typography>
+      <Divider sx={{ mb: 5 }} />
 
-    <Typography sx={{ color: '#0090a8', fontSize: 40, textAlign: 'center', mt: 5}}>
-        Rute
-    </Typography>
-    <Divider sx={{ mb: 5 }} />
-
-    <PilotFlyplassTo update={updateAirportTo} />
-    <PilotVelgDepArvl updateTil={(tid:string) => {setAnkomsttid(tid)}} updateFra={(tid:string) => {setAvgangstid(tid)}} />
-    <Divider sx={{ mb: 5 }} />
+      <PilotFlyplassTo update={updateAirportTo} />
+      <PilotVelgDepArvl updateTil={(tid:string) => {setAnkomsttid(tid)}} updateFra={(tid:string) => {setAvgangstid(tid)}} />
+      <Divider sx={{ mb: 5 }} />
         <div style={{textAlign: 'center', color: '#0090a8', marginBottom: '1em'}}>
-            <Typography sx={{ mb: 3 }} variant="h4">Taf metar</Typography>
-            <div className={styles.tafmetar}>
-                { airport !== undefined && 
-                        <TafMetar icao={airport.icao} />
-                }
-                {toAirportRedux !== undefined &&
-                  <TafMetar icao={toAirportRedux.icao} />
-                }
-            </div>
+          <Typography sx={{ mb: 3 }} variant="h4">Taf metar</Typography>
+          <div className={styles.tafmetar}>
+            { airport !== undefined && 
+              <TafMetar icao={airport.icao} />
+            }
+            {toAirportRedux !== undefined &&
+              <TafMetar icao={toAirportRedux.icao} />
+            }
+          </div>
         </div>
-    <Divider sx={{ mb: 5 }} />
-
+      <Divider sx={{ mb: 5 }} />
       <div style={{ display: 'flex', justifyContent: 'space-evenly', flexFlow: 'row wrap', alignItems: 'stretch', flexGrow: '1'}}>
         {terskel !== undefined && 
-        <VisSatteTerskelverdier terskel={terskel} />}
-      </div>
-      
-      
-    </Container>
-
-    <div style={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', flexFlow: 'row wrap', backgroundColor: '#dff2f6'}}>
-
-        <div style={{display: 'flex', maxWidth: '450px', minWidth: '280px', marginBottom: '1em'}}>
-          {airport !== undefined && fromWeather !== undefined && 
-            <GrafikkPilot airport={airport} weather={fromWeather} time={avgangstid} nextDay={false}/>
-          }
-        </div>
-        { toAirport !== undefined && weatherToAirport !== undefined &&
-            <div style={{ display: 'flex', maxWidth: '450px', minWidth: '280px', marginBottom: '1em'}}>
-                <GrafikkPilot airport={toAirport} weather={weatherToAirport} time={ankomsttid} nextDay={nesteDag} />
-            </div>
+          <VisSatteTerskelverdier terskel={terskel} />
         }
       </div>
-
-
-    </ >
+    </Container>
+    <div style={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', flexFlow: 'row wrap', backgroundColor: '#dff2f6'}}>
+      <div style={{display: 'flex', maxWidth: '450px', minWidth: '280px', marginBottom: '1em'}}>
+        {airport !== undefined && fromWeather !== undefined && 
+          <GrafikkPilot airport={airport} weather={fromWeather} time={avgangstid} nextDay={false}/>
+        }
+      </div>
+      { toAirport !== undefined && weatherToAirport !== undefined &&
+        <div style={{ display: 'flex', maxWidth: '450px', minWidth: '280px', marginBottom: '1em'}}>
+          <GrafikkPilot airport={toAirport} weather={weatherToAirport} time={ankomsttid} nextDay={nesteDag} />
+        </div>
+      }
+    </div>
+  </ >
   )
 }
 
