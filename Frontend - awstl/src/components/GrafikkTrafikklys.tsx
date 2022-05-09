c04e0e96cfb06b5677f9d406fdc01d110f5b9c83
Erik Myhre <myherik@gmail.com>
2022-05-09 12:40:29 +0200

    Rydding i GrafikkTrafikklys

--- Frontend - awstl/src/components/GrafikkTrafikklys.tsx
+++ Frontend - awstl/src/components/GrafikkTrafikklys.tsx
@@ -10,20 +10,22 @@ import { crosswind } from '../util/calcCrosswind';
 
 function GrafikkTrafikklys() {
 
-
-
-
     const weather = useSelector((state:any) => state.grafikk.value)
     let airport = useSelector((state:any) => state.airport.value)
     const terskel = useSelector((state: any) => state.terskel.value);
 
-    if (airport === undefined) {
-        airport = {icao: "ENDU", navn: "Bardufoss Lufthan"}
-    }
+    const cw = crosswind(airport?.rwy!!, weather?.data.instant.details.wind_from_direction, weather?.data.instant.details.wind_speed);
+
+    let precipitation_amount = 0;
+    let probThunder = 0
 
     const [temperatureColor, setTemperatureColor] = useState<any>(null)
     const [ikonpath, setIkonpath] = useState<string>("")
     const [nedbor, setNedbor] = useState<string>("")
+
+    if (airport === undefined) {
+        airport = {icao: "ENDU", navn: "Bardufoss Lufthan"}
+    }
 
     useEffect(() => {
         let ikonpath2:string =  "/weatherIcons/";
@@ -38,12 +40,6 @@ function GrafikkTrafikklys() {
         else if (weather?.data.next_12_hours != null) setNedbor(weather?.data.next_12_hours.details.precipitation_amount + "mm neste 12 timer")
     }, [weather])
     
-
-    const cw = crosswind(airport?.rwy!!, weather?.data.instant.details.wind_from_direction, weather?.data.instant.details.wind_speed);
-
-
-    let precipitation_amount = 0;
-    let probThunder = 0
     if ( weather?.data.next_1_hours !== undefined) {
         precipitation_amount =  weather?.data?.next_1_hours?.details?.precipitation_amount
         probThunder = weather?.data.next_1_hours?.details?.probability_of_thunder
@@ -70,76 +66,71 @@ function GrafikkTrafikklys() {
             </div>
         }
         
-            
-
         <div style={{display: 'flex', justifyContent: 'space-evenly', flexWrap:'wrap'}}>
-        <div style={{ width: '33%', minWidth: 'fit-content'}}>
-            <Typography gutterBottom style={{ fontSize: 20, color: "#0090a8", display: 'flex', alignItems:'center'}}>
-                Effektiv lufttemperatur: {weather?.data.instant.details.effective_temperature} °C
-                { // color : weather?.data.instant.details.air_temperature < terskel?.airTempMin ? "red" :  weather?.data.instant.details.air_temperature > terskel?.airTempMax ? "#0090a8" : "#FFAF42"
-                    weather?.data.instant.details.effective_temperature < terskel?.airTempMax &&
-                        <Hex color={weather?.data.instant.details.effective_temperature < terskel?.airTempMin ? "red" : "#FFAF42"} />
-                  }
-            </Typography>
-            <Typography gutterBottom style={{ fontSize: 20, color:"#0090a8", display: 'flex', alignItems:'center'}}>
-                Nedbør: {isNaN(precipitation_amount) ? "N/A" : precipitation_amount?.toPrecision(2) + "mm"}
-                {
-                    precipitation_amount > terskel?.precipitationMin &&
-                        <Hex color={precipitation_amount > terskel?.precipitationMax ? "red" : "#FFAF42"} />
-                }
-            </Typography>
-            <Typography gutterBottom style={{ fontSize: 20, color:"#0090a8",  display: 'flex', alignItems:'center'}}>
-                Vindhastighet: {weather?.data.instant.details.wind_speed} kt
-                { weather?.data.instant.details.wind_speed > terskel?.windSpeedMin &&
-                    <Hex color={weather?.data.instant.details.wind_speed > terskel?.windSpeedMax ? "red" : "#FFAF42"} />
+            <div style={{ width: '33%', minWidth: 'fit-content'}}>
+                <Typography gutterBottom style={{ fontSize: 20, color: "#0090a8", display: 'flex', alignItems:'center'}}>
+                    Effektiv lufttemperatur: {weather?.data.instant.details.effective_temperature} °C
+                    { // color : weather?.data.instant.details.air_temperature < terskel?.airTempMin ? "red" :  weather?.data.instant.details.air_temperature > terskel?.airTempMax ? "#0090a8" : "#FFAF42"
+                        weather?.data.instant.details.effective_temperature < terskel?.airTempMax &&
+                            <Hex color={weather?.data.instant.details.effective_temperature < terskel?.airTempMin ? "red" : "#FFAF42"} />
+                    }
+                </Typography>
+                <Typography gutterBottom style={{ fontSize: 20, color:"#0090a8", display: 'flex', alignItems:'center'}}>
+                    Nedbør: {isNaN(precipitation_amount) ? "N/A" : precipitation_amount?.toPrecision(2) + "mm"}
+                    {
+                        precipitation_amount > terskel?.precipitationMin &&
+                            <Hex color={precipitation_amount > terskel?.precipitationMax ? "red" : "#FFAF42"} />
+                    }
+                </Typography>
+                <Typography gutterBottom style={{ fontSize: 20, color:"#0090a8",  display: 'flex', alignItems:'center'}}>
+                    Vindhastighet: {weather?.data.instant.details.wind_speed} kt
+                    { weather?.data.instant.details.wind_speed > terskel?.windSpeedMin &&
+                        <Hex color={weather?.data.instant.details.wind_speed > terskel?.windSpeedMax ? "red" : "#FFAF42"} />
 
-                }
-            </Typography>
-            <Typography gutterBottom style={{ fontSize: 20, display: 'flex', alignItems:'center', color: "#0090a8"}}>
-                Vindkast: {(isNaN(weather?.data.instant.details.wind_speed_of_gust) === true ? "N/A" : weather?.data.instant.details.wind_speed_of_gust + " kt")}
-                { weather?.data.instant.details.wind_speed_of_gust > terskel?.windGustMin &&
-                    <Hex color={ weather?.data.instant.details.wind_speed_of_gust > terskel?.windGustMax ? "red" : "#FFAF42"} />
-                }
-            </Typography>
-            <Typography gutterBottom style={{ fontSize: 20, display: 'flex', alignItems:'center', color : "#0090a8"}}>
-                Sannsynlighet torden: {(probThunder <= 100 && probThunder >= 0) ? probThunder + "%" : "N/A"}
-                { probThunder >= terskel?.probThunderMin &&
-                    <Hex color={probThunder > terskel?.probThunderMax ? "red" : "FFAF42"} />
+                    }
+                </Typography>
+                <Typography gutterBottom style={{ fontSize: 20, display: 'flex', alignItems:'center', color: "#0090a8"}}>
+                    Vindkast: {(isNaN(weather?.data.instant.details.wind_speed_of_gust) === true ? "N/A" : weather?.data.instant.details.wind_speed_of_gust + " kt")}
+                    { weather?.data.instant.details.wind_speed_of_gust > terskel?.windGustMin &&
+                        <Hex color={ weather?.data.instant.details.wind_speed_of_gust > terskel?.windGustMax ? "red" : "#FFAF42"} />
+                    }
+                </Typography>
+                <Typography gutterBottom style={{ fontSize: 20, display: 'flex', alignItems:'center', color : "#0090a8"}}>
+                    Sannsynlighet torden: {(probThunder <= 100 && probThunder >= 0) ? probThunder + "%" : "N/A"}
+                    { probThunder >= terskel?.probThunderMin &&
+                        <Hex color={probThunder > terskel?.probThunderMax ? "red" : "FFAF42"} />
 
-                }
-            </Typography>
-            <Typography gutterBottom style={{ fontSize: 20, display: 'flex', alignItems:'center', color : "#0090a8"}}>
-                Luftfuktighet: {weather?.data.instant.details.relative_humidity}%
-                { weather?.data.instant.details.relative_humidity > terskel?.humidityMin &&
-                    <Hex color={weather?.data.instant.details.relative_humidity > terskel?.humidityMax ? "red" : "#FFAF42"} />
+                    }
+                </Typography>
+                <Typography gutterBottom style={{ fontSize: 20, display: 'flex', alignItems:'center', color : "#0090a8"}}>
+                    Luftfuktighet: {weather?.data.instant.details.relative_humidity}%
+                    { weather?.data.instant.details.relative_humidity > terskel?.humidityMin &&
+                        <Hex color={weather?.data.instant.details.relative_humidity > terskel?.humidityMax ? "red" : "#FFAF42"} />
 
-                }
-            </Typography>
-            <Typography gutterBottom style={{ fontSize: 20, display: 'flex', alignItems:'center', color : "#0090a8"}}>
-                Crosswind: {cw.toPrecision(2)} kt
-                { cw > terskel?.crosswindMin &&
-                    <Hex color={cw > terskel?.crosswindMax ? "red" : "#FFAF42"} />
+                    }
+                </Typography>
+                <Typography gutterBottom style={{ fontSize: 20, display: 'flex', alignItems:'center', color : "#0090a8"}}>
+                    Crosswind: {cw.toPrecision(2)} kt
+                    { cw > terskel?.crosswindMin &&
+                        <Hex color={cw > terskel?.crosswindMax ? "red" : "#FFAF42"} />
 
-                }
-            </Typography>
-        </div>
-        <div style={{ width: '33%', minWidth: 'fit-content' }}>
-                    {/** Ikonet */}
-                    {weather !== undefined &&
-                        <div style={{width: "100%", display: 'flex', flexDirection: 'column', flexWrap:'wrap', justifyContent: 'center'}}>
-                            
-                            <Box style={{ display: 'flex', justifyContent: 'center', maxHeight: '100px'}} >
-                                <img style={{width: '50%'}} src={ikonpath} alt={ikonpath} />
-                            </Box>
-
-                            <Box>
-                                <div style={{ display: 'flex', justifyContent: 'center'}}>
-                                    <Typography sx={{color: `${temperatureColor}`, fontSize: 38}}>
-                                        {weather.data.instant.details.air_temperature}°C
-                                    </Typography>
-                                </div>
-                            </Box>
-
+                    }
+                </Typography>
+            </div>
+            <div style={{ width: '33%', minWidth: 'fit-content' }}>
+                {/** Ikonet */}
+                {weather !== undefined &&
+                    <div style={{width: "100%", display: 'flex', flexDirection: 'column', flexWrap:'wrap', justifyContent: 'center'}}> 
+                        <Box style={{ display: 'flex', justifyContent: 'center', maxHeight: '100px'}} >
+                            <img style={{width: '50%'}} src={ikonpath} alt={ikonpath} />
+                        </Box>
+                        <Box>
+                            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                                <Typography sx={{color: `${temperatureColor}`, fontSize: 38}}>
+                                    {weather.data.instant.details.air_temperature}°C
+                                </Typography>
+                            </div>
+                        </Box>
                         <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', color: '#0090a8'}}>
                             <Typography>
                                 {weather.data.instant.details.wind_from_direction < 0
@@ -149,76 +140,63 @@ function GrafikkTrafikklys() {
                             {nedbor !== "" &&
                                 <Typography>{nedbor}</Typography>
                             }
-
                         </div>
-                </div>
-            }
-            
-            {weather === undefined &&
-
-                <div>
-                    <h2>Vennligst vent</h2>
-                </div>
-            }
-        </div>
-        <div style={{ width: '33%', minWidth: 'fit-content', position: 'relative' }}>
-            <img alt="compass rose" src="/Gray_compass_rose.svg" style={{height: '40px', position: 'absolute', bottom: 0, right: 0}}/>
-            {/** Ikonet */}
-                    {weather !== undefined &&
-                        <div style={{width: "100%", display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-
+                    </div>
+                }
+                {weather === undefined &&
+                    <div>
+                        <h2>Vennligst vent</h2>
+                    </div>
+                }
+            </div>
+            <div style={{ width: '33%', minWidth: 'fit-content', position: 'relative' }}>
+                <img alt="compass rose" src="/Gray_compass_rose.svg" style={{height: '40px', position: 'absolute', bottom: 0, right: 0}}/>
+                {/** Ikonet */}
+                {weather !== undefined &&
+                    <div style={{width: "100%", display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
                         <div style={{ display: 'flex', justifyContent: 'center'}}>
                             <Typography gutterBottom style={{ fontSize: 20, color: '#0090a8'}}>
                                 Vind i forhold til rullebane
                             </Typography>
                         </div>
-
-                    <Box style={{ display: 'flex', justifyContent: 'space-evenly', flexDirection: 'row', alignItems: 'center'}}>
-                        
-                        <div style={{ height: '230px', maxWidth: '100%', width: '230px', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative'}}>
-                            <div style={{display: 'flex', flexWrap: 'wrap', minWidth: '215px'}}>
-                            {
-
+                        <Box style={{ display: 'flex', justifyContent: 'space-evenly', flexDirection: 'row', alignItems: 'center'}}> 
+                            <div style={{ height: '230px', maxWidth: '100%', width: '230px', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative'}}>
+                                <div style={{display: 'flex', flexWrap: 'wrap', minWidth: '215px'}}>
+                                {
                                     airport.rwy.split(",").map((it: any) => {
-
                                         return <Rullebane key={it[0]} rullebane={it}></Rullebane>
-
                                     })
-
-                            }
-                            </div>
-                            <div style={{position: 'absolute',
-                                right: (weather.data.instant.details.wind_from_direction > 0 && weather.data.instant.details.wind_from_direction < 180) ||
-                                    (weather.data.instant.details.wind_from_direction < -360 && weather.data.instant.details.wind_from_direction > -180) ?
-                                    "0px" : "90%",
-                                top: (weather.data.instant.details.wind_from_direction > 90 && weather.data.instant.details.wind_from_direction < 270) ||
-                                    (weather.data.instant.details.wind_from_direction > -270 && weather.data.instant.details.wind_from_direction < -90) ?
-                                    "150px" : "0px",
-                                textAlign: 'center',
-                                color: '#0090a8'
-                            }}>
-
-                                <Typography style={{position: 'absolute', top: -5,left: "calc(50%-100px)", zIndex: 1, }}>
-                                    {weather.data.instant.details.wind_speed}kt
-                                </Typography>
-                                <div style={{ position: 'relative', zIndex: 0,
-                                transform: `rotate(${weather.data.instant.details.wind_from_direction + 90}deg)`}}>
-                                    <ArrowRightAltIcon sx={{ fontSize: 40 }}></ArrowRightAltIcon>
+                                }
                                 </div>
-
+                                <div style={{position: 'absolute',
+                                    right: (weather.data.instant.details.wind_from_direction > 0 && weather.data.instant.details.wind_from_direction < 180) ||
+                                        (weather.data.instant.details.wind_from_direction < -360 && weather.data.instant.details.wind_from_direction > -180) ?
+                                        "0px" : "90%",
+                                    top: (weather.data.instant.details.wind_from_direction > 90 && weather.data.instant.details.wind_from_direction < 270) ||
+                                        (weather.data.instant.details.wind_from_direction > -270 && weather.data.instant.details.wind_from_direction < -90) ?
+                                        "150px" : "0px",
+                                    textAlign: 'center',
+                                    color: '#0090a8'
+                                }}>
+                                    <Typography style={{position: 'absolute', top: -5,left: "calc(50%-100px)", zIndex: 1, }}>
+                                        {weather.data.instant.details.wind_speed}kt
+                                    </Typography>
+                                    <div style={{ position: 'relative', zIndex: 0,
+                                    transform: `rotate(${weather.data.instant.details.wind_from_direction + 90}deg)`}}>
+                                        <ArrowRightAltIcon sx={{ fontSize: 40 }}></ArrowRightAltIcon>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </Box>
-                </div>
-            }
-            
-            {weather === undefined &&
-
-                <div>
-                    <h2>Venligst vent</h2>
-                </div>
-            }
-        </div>
+                        </Box>
+                    </div>
+                }
+                
+                {weather === undefined &&
+                    <div>
+                        <h2>Venligst vent</h2>
+                    </div>
+                }
+            </div>
         </div>
         </Container>
         </div>
