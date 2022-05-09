1867b992a742f47f585394a630f13709643caf53
Erik Myhre <myherik@gmail.com>
2022-05-09 12:40:04 +0200

    Rydding i GrafikkPilot

--- Frontend - awstl/src/components/GrafikkPilot.tsx
+++ Frontend - awstl/src/components/GrafikkPilot.tsx
@@ -12,38 +12,34 @@ import { crosswind } from '../util/calcCrosswind';
 function GrafikkPilot(props: {airport:airports, weather:LocationForecast, time: string, nextDay: boolean}) {
 
     const airport = props.airport;
-    
-  
+
+    let cw:number;
+    let precipitation_amount = 0;
+    let probThunder = 0
+
     const [weather, setWeather] = useState<Timesery | null>(null)
+    const [temperatureColor, setTemperatureColor] = useState<any>(null)
+    const [ikonpath, setIkonpath] = useState<string>("")
+    const [nedbor, setNedbor] = useState<string>("")
+
+    const terskel = useSelector((state: any) => state.terskel.value);
 
     useEffect(() => {
-
-          let teller = 0;
-          if ( props.weather?.properties?.timeseries !== undefined){
+        let teller = 0;
+        if ( props.weather?.properties?.timeseries !== undefined){
             for (const it of props.weather.properties.timeseries) {
                 if (new Date(it.time.toString()).toLocaleString().split(", ")[1].split(":")[0] == props.time.split(":")[0]) {
-                        if (teller === 0 && props.nextDay){
-                           teller++; 
-                        } else {
-                            setWeather(it)
-                            break
-                        }
-                      
+                    if (teller === 0 && props.nextDay){
+                        teller++; 
+                    } else {
+                        setWeather(it)
+                        break
+                    }
                 }
             }            
         }
           
     }, [props.time, props.nextDay])
-
-
-    const terskel = useSelector((state: any) => state.terskel.value);
-
-
-    const [temperatureColor, setTemperatureColor] = useState<any>(null)
-    const [ikonpath, setIkonpath] = useState<string>("")
-    const [nedbor, setNedbor] = useState<string>("")
-
-    let cw:number;
 
     useEffect(() => {
         if (weather != undefined) {
@@ -67,16 +63,12 @@ function GrafikkPilot(props: {airport:airports, weather:LocationForecast, time: 
 
     } else {return <div>Feil i grafikk</div>}
     
-    let precipitation_amount = 0;
-    let probThunder = 0
     if ( weather?.data.next_1_hours !== undefined) {
         precipitation_amount =  weather?.data?.next_1_hours?.details?.precipitation_amount
         probThunder = weather?.data.next_1_hours?.details?.probability_of_thunder
     } else if (weather?.data.next_6_hours !== undefined) {
         precipitation_amount =  weather?.data?.next_6_hours?.details?.precipitation_amount / 6
     }
-
-
 
   return (
     <>
@@ -92,13 +84,10 @@ function GrafikkPilot(props: {airport:airports, weather:LocationForecast, time: 
                 <Divider sx={{ mb: 5 }} />
             </div>
         }
-        
-            
 
         <div style={{display: 'flex', justifyContent: 'space-evenly', flexWrap:'wrap'}}>
             <div style={{ width: '100%', minWidth: 'fit-content', marginBottom: '1em'}}>
               
-
                 <Typography gutterBottom style={{ fontSize: 20, color: "#0090a8", display: 'flex', justifyContent: 'center', alignItems:'center'}}>
                     Effektiv lufttemperatur: {weather?.data.instant.details.effective_temperature} °C
                     { // color : weather?.data.instant.details.air_temperature < terskel?.airTempMin ? "red" :  weather?.data.instant.details.air_temperature > terskel?.airTempMax ? "#0090a8" : "#FFAF42"
@@ -106,6 +95,7 @@ function GrafikkPilot(props: {airport:airports, weather:LocationForecast, time: 
                         <Hex color={weather?.data.instant.details.effective_temperature!! < terskel?.airTempMin ? "red" : "#FFAF42"} />
                     }
                 </Typography>
+
                 <Typography gutterBottom style={{ fontSize: 20, color:"#0090a8", display: 'flex', justifyContent: 'center', alignItems:'center'}}>
                     Nedbør: {isNaN(precipitation_amount) ? "N/A" : precipitation_amount?.toPrecision(2) + "mm"}
                     {
@@ -150,38 +140,36 @@ function GrafikkPilot(props: {airport:airports, weather:LocationForecast, time: 
 
             </div>
         <div style={{ width: '100%', minWidth: 'fit-content' }}>
-                    {/** Ikonet */}
-                    {weather !== undefined &&
-                        <div style={{width: "100%", display: 'flex', flexDirection: 'column', flexWrap:'wrap', justifyContent: 'center'}}>
+            {/** Ikonet */}
+            {weather !== undefined &&
+                <div style={{width: "100%", display: 'flex', flexDirection: 'column', flexWrap:'wrap', justifyContent: 'center'}}>
                             
-                            <Box style={{ display: 'flex', justifyContent: 'center', maxHeight: '100px'}} >
-                                <img style={{width: '50%'}} src={ikonpath} alt={ikonpath} />
-                            </Box>
+                    <Box style={{ display: 'flex', justifyContent: 'center', maxHeight: '100px'}} >
+                        <img style={{width: '50%'}} src={ikonpath} alt={ikonpath} />
+                    </Box>
 
-                            <Box>
-                                <div style={{ display: 'flex', justifyContent: 'center'}}>
-                                    <Typography sx={{color: `${temperatureColor}`, fontSize: 38}}>
-                                        {weather.data.instant.details.air_temperature}°C
-                                    </Typography>
-                                </div>
-                            </Box>
-
-                        <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', color: '#0090a8'}}>
-                            <Typography>
-                                {weather.data.instant.details.wind_from_direction < 0
-                                    ? 360 + weather.data.instant.details.wind_from_direction
-                                    : weather.data.instant.details.wind_from_direction}°
+                    <Box>
+                        <div style={{ display: 'flex', justifyContent: 'center'}}>
+                            <Typography sx={{color: `${temperatureColor}`, fontSize: 38}}>
+                                {weather.data.instant.details.air_temperature}°C
                             </Typography>
-                            {nedbor !== "" &&
-                                <Typography sx={{ mb: 5}}>{nedbor}</Typography>
-                            }
-
                         </div>
+                    </Box>
+
+                    <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', color: '#0090a8'}}>
+                        <Typography>
+                            {weather.data.instant.details.wind_from_direction < 0
+                                ? 360 + weather.data.instant.details.wind_from_direction
+                                : weather.data.instant.details.wind_from_direction}°
+                        </Typography>
+                        {nedbor !== "" &&
+                            <Typography sx={{ mb: 5}}>{nedbor}</Typography>
+                        }
+                    </div>
                 </div>
             }
             
             {weather === undefined &&
-
                 <div>
                     <h2>Vennligst vent</h2>
                 </div>
@@ -190,23 +178,21 @@ function GrafikkPilot(props: {airport:airports, weather:LocationForecast, time: 
         <div style={{ width: '100%', minWidth: 'fit-content', position: 'relative' }}>
             <img alt="compass rose" src="/Gray_compass_rose.svg" style={{height: '40px', position: 'absolute', bottom: 0, right: 0}}/>
             {/** Ikonet */}
-                    {weather !== undefined && 
-                        <div style={{width: "100%", display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                {weather !== undefined && 
+                    <div style={{width: "100%", display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
 
-                        <div style={{ display: 'flex', justifyContent: 'center'}}>
-                            <Typography gutterBottom style={{ fontSize: 20, color: '#0090a8'}}>
-                                Vind i forhold til rullebane
-                            </Typography>
-                        </div>
+                    <div style={{ display: 'flex', justifyContent: 'center'}}>
+                        <Typography gutterBottom style={{ fontSize: 20, color: '#0090a8'}}>
+                            Vind i forhold til rullebane
+                        </Typography>
+                    </div>
 
                     <Box style={{ display: 'flex', justifyContent: 'space-evenly', flexDirection: 'row', alignItems: 'center'}}>
                         
                         <div style={{ height: '230px', width: '230px', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative'}}>
                             { 
                                 airport.rwy?.split(",").map((it: any) => {
-
                                     return <Rullebane key={it[0]} rullebane={it}></Rullebane>
-
                                 })
                             }
 
@@ -228,7 +214,6 @@ function GrafikkPilot(props: {airport:airports, weather:LocationForecast, time: 
                                 transform: `rotate(${weather.data.instant.details.wind_from_direction + 90}deg)`}}>
                                     <ArrowRightAltIcon sx={{ fontSize: 40 }}></ArrowRightAltIcon>
                                 </div>
-
                             </div>
                         </div>
                     </Box>
@@ -236,7 +221,6 @@ function GrafikkPilot(props: {airport:airports, weather:LocationForecast, time: 
             }
             
             {weather === undefined &&
-
                 <div>
                     <h2>Venligst vent</h2>
                 </div>
@@ -244,7 +228,6 @@ function GrafikkPilot(props: {airport:airports, weather:LocationForecast, time: 
             </div>
         </div>
         </div>
-
     </>
   )
 }
