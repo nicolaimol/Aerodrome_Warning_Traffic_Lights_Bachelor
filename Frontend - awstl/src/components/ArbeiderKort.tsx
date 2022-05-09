fb430409ee3a2c3a75a2a657d54f691b32ae03ab
Erik Myhre <myherik@gmail.com>
2022-05-09 12:38:59 +0200

    Rydding i

--- Frontend - awstl/src/components/ArbeiderKort.tsx
+++ Frontend - awstl/src/components/ArbeiderKort.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import styles from '../style/Arbeiderkort.module.css'
-// Interface
 import arbeiderInfo from '../model/arbeiderInfo'
-
-// Material UI
 import { Card, CardContent, Typography, Box, Button } from '@mui/material'
-
 import {Link} from 'react-router-dom'
 
 
@@ -14,32 +10,26 @@ function ArbeiderKort(props:arbeiderInfo) { {/** Tar inn props med interface til
   return (
       
         <Card className={styles.kortButton} sx={{ backgroundColor: '#0494ac'}} style={{display: 'flex', flexGrow: 1}}>
-                <Link to={'/' + props.tittel.toLowerCase()}>
+            <Link to={'/' + props.tittel.toLowerCase()}>
                 <CardContent style={{ display: 'flex', flexDirection: 'column', flexGrow: 1}}>
                     {/** Tittel */}
                     <div style={{display: 'flex', flexDirection: 'column', flexGrow: 1}}>
                         <Typography sx={{ color: 'white' }} variant="h5">
-                        {props.tittel}
+                            {props.tittel}
                         </Typography>
                         {/** Undertittel */}
                         <Typography sx={{ mb: 10, color: "white"}}>
                             {props.infoTekst}
                         </Typography>
                     </div>
-                    
+                        
                     {/** Ikon */}
                     <Box sx={{ mb: 10, color: 'white' }} textAlign='center'>
                         {props.ikonComp}
                     </Box>
                     {/** Knapp */}
-                    {/**
-                    <Box textAlign='center'>
-                    <Link to={'/' + props.tittel.toLowerCase()}><Button className={styles.kortButton} variant="contained" >Sjekk</Button></Link>
-
-                    </Box>
-                    */}
                 </CardContent>
-                </Link>
+            </Link>
         </Card>
     
   )
