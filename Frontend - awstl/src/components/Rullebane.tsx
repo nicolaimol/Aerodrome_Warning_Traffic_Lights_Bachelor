753d942d5294294276194f6a27e24504a49830b6
Erik Myhre <myherik@gmail.com>
2022-05-09 12:42:54 +0200

    Rydding i Rullebane

--- Frontend - awstl/src/components/Rullebane.tsx
+++ Frontend - awstl/src/components/Rullebane.tsx
@@ -2,26 +2,25 @@ import React from 'react';
 
 function Rullebane(props: any) {
     return (
-            <div style={{backgroundColor: '#404040',
-                            color: 'yellow',
-                            display: 'inline-flex',
-                            justifyContent: 'space-between',
-                            padding: 6,
-                            border: "1px yellow solid",
-                            width: 'max-content',
-                            transform: `rotate(${Number(props.rullebane.split("/")[0])*10 - 90}deg)`,
-                            }}>
-                <div style={{transform: 'rotate(90deg)', width: 'fit-content'}}>
-                    {props.rullebane.split("/")[0]}
-                </div>
-                <span style={{width: 'max-content'}}>
-                    - - - - - - - - - - - - - - - -
-                </span>
-                <div style={{transform: 'rotate(-90deg)', width: 'fit-content'}}>
-                    {props.rullebane.split("/")[1]}
-                </div>
-
+        <div style={{backgroundColor: '#404040',
+                    color: 'yellow',
+                    display: 'inline-flex',
+                    justifyContent: 'space-between',
+                    padding: 6,
+                    border: "1px yellow solid",
+                    width: 'max-content',
+                    transform: `rotate(${Number(props.rullebane.split("/")[0])*10 - 90}deg)`,
+                    }}>
+            <div style={{transform: 'rotate(90deg)', width: 'fit-content'}}>
+                {props.rullebane.split("/")[0]}
             </div>
+            <span style={{width: 'max-content'}}>
+                - - - - - - - - - - - - - - - -
+            </span>
+            <div style={{transform: 'rotate(-90deg)', width: 'fit-content'}}>
+                {props.rullebane.split("/")[1]}
+            </div>
+        </div>
     );
 }
 
