20871c150063f924eff8b59ca23d32dafbfe9e50
s341833 <56063595+s341833@users.noreply.github.com>
2022-05-09 14:00:56 +0200

    calc crosswind now works for more than one runway, uses Math.min

--- Frontend - awstl/src/util/calcCrosswind.ts
+++ Frontend - awstl/src/util/calcCrosswind.ts
@@ -1,19 +1,29 @@
-export const crosswind = (rwy: string, windDirection: number, windSpeed: number) => {
+export const crosswind = (rwy: string, windDirection: number, windSpeed: number): number => {
 
-    const windDir = windDirection > 0 ?
-        windDirection :
-        360 + windDirection
+    if (rwy.split(",").length > 1){
 
-    const rwyDiff1 = Math.abs(10*Number(rwy?.split("/")[0]) - windDir)
-    const rwyDiff2 = Math.abs(10*Number(rwy?.split("/")[1]) - windDir)
+        return Math.max(...rwy.split(",").map((it: any) => {
+            return crosswind(it, windDirection, windSpeed)
+        }))
+        /*return Math.min(
+            crosswind(rwy.split(",")[0], windDirection, windSpeed),
+            crosswind(rwy.split(",")[1], windDirection, windSpeed)
+        )*/
+    } else {
+        const windDir = windDirection > 0 ?
+            windDirection :
+            360 + windDirection
 
-    const cw = windSpeed * Math.sin
-    (   (Math.min(
-        rwyDiff1 ,
-        rwyDiff2
-    ) / 360) * 2 * Math.PI
-    )
+        const rwyDiff1 = Math.abs(10*Number(rwy?.split("/")[0]) - windDir)
+        const rwyDiff2 = Math.abs(10*Number(rwy?.split("/")[1]) - windDir)
 
-    return cw
+        const cw = windSpeed * Math.sin
+        (   (Math.min(
+            rwyDiff1 ,
+            rwyDiff2
+        ) / 360) * 2 * Math.PI
+        )
 
+        return cw
+    }
 }
