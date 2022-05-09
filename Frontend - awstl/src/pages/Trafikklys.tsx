ee984f7c183d5aa334748f71e9c3d6d0af04ff4b
Erik Myhre <myherik@gmail.com>
2022-05-09 12:45:12 +0200

    Rydding i Trafikklys

--- Frontend - awstl/src/pages/Trafikklys.tsx
+++ Frontend - awstl/src/pages/Trafikklys.tsx
@@ -22,57 +22,44 @@ function Trafikklys() {
   const [hjemMobil, setHjemMobil] = React.useState(false);
 
   useEffect(() => {
+
     const temp = nowcast?.nowcasts[0].properties.timeseries[0].data.instant.details.air_temperature
-
     setColor(temp > terskel?.airTemp ? "green" : temp == terskel?.airTemp ? "yellow" : "red")
-
     setColor(calcFarge(nowcast?.nowcasts[0].properties.timeseries[0].data.instant.details, terskel!!, airport!!,
         {
-            precipitation_amount: 0,
-            probThunder: 0
+          precipitation_amount: 0,
+          probThunder: 0
         }))
-
-
   }, [terskel, nowcast])
 
   useEffect(() => {
-
     (window.innerWidth > 849) ? setHjemMobil(false) : setHjemMobil(true);
-  
-          window.addEventListener('resize', () => {
-              (window.innerWidth > 849) ? setHjemMobil(false) : setHjemMobil(true);
-          });
-  
-          return () => window.removeEventListener('resize', () => {});
-      }, []);
+      window.addEventListener('resize', () => {
+        (window.innerWidth > 849) ? setHjemMobil(false) : setHjemMobil(true);
+      });  
+      return () => window.removeEventListener('resize', () => {});
+    }, []);
 
   return (
     <>
-    <Container>
-
-    <Typography sx={{ color: '#0090a8', fontSize: hjemMobil ? 30 : 40, textAlign: 'center', mt: 5}}>
+      <Container>
+        <Typography sx={{ color: '#0090a8', fontSize: hjemMobil ? 30 : 40, textAlign: 'center', mt: 5}}>
           Flyplass{hjemMobil ? (' - ' + airport?.navn) : ""}
         </Typography>
         <Divider sx={{ mb: 5 }} />
-
-      <div style={{ display: 'flex', justifyContent: 'space-evenly', flexFlow: 'row wrap', alignItems: 'stretch'}}>
-
-      <TrafikklysBox farge={color} />  
-      {terskel != undefined &&
-        <VisSatteTerskelverdier terskel={terskel} />}
-        
-      </div>
-
-      
-
-      <div>
-            <Typography sx={{ color: '#0090a8', fontSize: 30, textAlign: 'center'}}>
-                Tidslinje for de neste 72 timer
-            </Typography>
-            <Divider sx={{ mb: 5}} />
-        <Tidslinje />
-      </div>
-    </Container>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly', flexFlow: 'row wrap', alignItems: 'stretch'}}>
+          <TrafikklysBox farge={color} />  
+          {terskel != undefined &&
+            <VisSatteTerskelverdier terskel={terskel} />} 
+        </div>
+        <div>
+          <Typography sx={{ color: '#0090a8', fontSize: 30, textAlign: 'center'}}>
+            Tidslinje for de neste 72 timer
+          </Typography>
+          <Divider sx={{ mb: 5}} />
+          <Tidslinje />
+        </div>
+      </Container>
       <GrafikkTrafikklys />
     </>
   )
