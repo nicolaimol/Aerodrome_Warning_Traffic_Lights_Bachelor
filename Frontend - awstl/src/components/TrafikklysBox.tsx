badda0b374b9d32beac0ebf54c7b0c2294b5d3f9
Erik Myhre <myherik@gmail.com>
2022-05-09 12:44:10 +0200

    Rydding i TrafikklysBox

--- Frontend - awstl/src/components/TrafikklysBox.tsx
+++ Frontend - awstl/src/components/TrafikklysBox.tsx
@@ -5,7 +5,56 @@ import trafikklysFarge from '../model/trafikklysFarge';
 
 function TrafikklysBox(props: trafikklysFarge) {
 
+    let aktivFarge;
+
     const [mobil, setMobil] = React.useState(false);
+
+    const greenLight =
+        <>
+                <Typography variant='h6' gutterBottom>
+                    Akkurat nå er det grønt lys
+                </Typography>
+                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'red', height: '100px', width: '100px', opacity: '50%', filter: 'grayscale(100%)'}}>
+                </Box>
+                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'yellow', height: '100px', width: '100px', filter: 'grayscale(100%)'}}>
+                </Box>
+                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'green', height: '100px', width: '100px', boxShadow: '0 0 20px green'}}>
+                </Box>
+        </>
+
+    const yellowLight =
+        <>
+                <Typography variant='h6' gutterBottom>
+                    Akkurat nå er det gult lys
+                </Typography>
+                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'red', height: '100px', width: '100px', opacity: '50%', filter: 'grayscale(100%)'}}>
+                </Box>
+                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'yellow', height: '100px', width: '100px', boxShadow: '0 0 20px yellow'}}>
+                </Box>
+                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'green', height: '100px', width: '100px', opacity: '50%', filter: 'grayscale(100%)'}}>
+                </Box>
+        </>
+
+    const redLight =
+        <>
+                <Typography variant='h6' gutterBottom>
+                    Akkurat nå er det rødt lys
+                </Typography>
+                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'red', height: '100px', width: '100px', boxShadow: '0 0 20px red'}}>
+                </Box>
+                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'yellow', height: '100px', width: '100px', filter: 'grayscale(100%)'}}>
+                </Box>
+                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'green', height: '100px', width: '100px', opacity: '50%', filter: 'grayscale(100%)'}}>
+                </Box>
+        </>
+
+    if (props.farge === 'green'){
+        aktivFarge = greenLight;
+    } else if (props.farge === 'yellow') {
+        aktivFarge = yellowLight;
+    } else {
+        aktivFarge = redLight;
+    }
 
     useEffect(() => {
 
@@ -16,67 +65,8 @@ function TrafikklysBox(props: trafikklysFarge) {
         });
 
         return () => window.removeEventListener('resize', () => {});
+    
     }, []);
-
-    const greenLight =
-            <>
-                <Typography variant='h6' gutterBottom>
-                    Akkurat nå er det grønt lys
-                </Typography>
-                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'red', height: '100px', width: '100px', opacity: '50%', filter: 'grayscale(100%)'}}>
-
-                </Box>
-                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'yellow', height: '100px', width: '100px', filter: 'grayscale(100%)'}}>
-
-                </Box>
-                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'green', height: '100px', width: '100px', boxShadow: '0 0 20px green'}}>
-
-                </Box>
-            </>
-
-    const yellowLight =
-        <>
-                <Typography variant='h6' gutterBottom>
-                    Akkurat nå er det gult lys
-                </Typography>
-                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'red', height: '100px', width: '100px', opacity: '50%', filter: 'grayscale(100%)'}}>
-
-                </Box>
-                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'yellow', height: '100px', width: '100px', boxShadow: '0 0 20px yellow'}}>
-
-                </Box>
-                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'green', height: '100px', width: '100px', opacity: '50%', filter: 'grayscale(100%)'}}>
-
-                </Box>
-        </>
-
-    const redLight =
-                <>
-                <Typography variant='h6' gutterBottom>
-                    Akkurat nå er det rødt lys
-                </Typography>
-                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'red', height: '100px', width: '100px', boxShadow: '0 0 20px red'}}>
-
-                </Box>
-                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'yellow', height: '100px', width: '100px', filter: 'grayscale(100%)'}}>
-
-                </Box>
-                <Box sx={{ mb: 5}} style={{borderRadius: '50%', backgroundColor: 'green', height: '100px', width: '100px', opacity: '50%', filter: 'grayscale(100%)'}}>
-
-                </Box>
-            </>
-
-
-
-    let aktivFarge;
-
-  if (props.farge === 'green'){
-    aktivFarge = greenLight;
-  } else if (props.farge === 'yellow') {
-    aktivFarge = yellowLight;
-  } else {
-    aktivFarge = redLight;
-  }
 
   return (
       <div style={{ display: 'flex', justifyContent: 'space-between', flexFlow: 'column wrap', width: '40%', color: '#0090a8', minWidth: '270px'}}>
@@ -84,7 +74,7 @@ function TrafikklysBox(props: trafikklysFarge) {
               Trafikklys
           </Typography>
           <Paper elevation={3} sx={{ mb: 5}} style={{ color: '#0090a8', minHeight: '20vh', display: 'flex', justifyContent: 'space-evenly', flexDirection: 'column', alignItems: 'center'}} >
-    {aktivFarge}
+            {aktivFarge}
           </Paper>
       </div>
   )
