a9fae6ae2bbe737d2173b0f2c62051887f1c7eba
s341833 <56063595+s341833@users.noreply.github.com>
2022-05-19 10:38:43 +0200

    cache unavailabe fallback

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/service/TafMetarService.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/service/TafMetarService.kt
@@ -1,9 +1,11 @@
 package bachelor.met.awstl.service
 
 import bachelor.met.awstl.dto.TafMetarDto
+import bachelor.met.awstl.enum.Cache
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.data.redis.RedisConnectionFailureException
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 
@@ -15,9 +17,17 @@ class TafMetarService(val httpService: HttpService, val service: FlyplassService
 
     val logger = LoggerFactory.getLogger(TafMetarService::class.java)
 
+    fun getTafMetar(icao: String): TafMetarDto {
+        try {
+            return getMetarCache(icao)
+        } catch (e: RedisConnectionFailureException) {
+            logger.error("Redis connection error: {}", e.message)
+            return getTafMetarDefault(icao)
+        }
+    }
 
     @Cacheable(value = ["tafmetar"], key = "#icao")
-    fun getMetar(icao: String): TafMetarDto {
+    fun getMetarCache(icao: String): TafMetarDto {
         logger.info("Getting taf metar for $icao")
 
         service.getFlyplass(icao)
@@ -25,7 +35,7 @@ class TafMetarService(val httpService: HttpService, val service: FlyplassService
         val query: HashMap<String, String> = HashMap()
         query["icao"] = icao
 
-        val tafmetar = httpService.hentData(url, String::class.java, query)
+        val tafmetar = httpService.hentData(url, String::class.java, query, icao, Cache.TAFMETAR)
 
         if (tafmetar != null) {
 
@@ -40,6 +50,30 @@ class TafMetarService(val httpService: HttpService, val service: FlyplassService
 
         throw Exception("Could not get taf metar for icao $icao")
 
+    }
+
+    private fun getTafMetarDefault(icao: String): TafMetarDto {
+        logger.info("Getting taf metar for $icao")
+
+        service.getFlyplass(icao)
+
+        val query: HashMap<String, String> = HashMap()
+        query["icao"] = icao
+
+        val tafmetar = httpService.hentData(url, String::class.java, query, icao, Cache.TAFMETAR)
+
+        if (tafmetar != null) {
+
+            val list = tafmetar.lines()
+            val ret = TafMetarDto()
+
+            ret.taf = list[3]
+            ret.metar = list[list.size-3]
+
+            return ret;
+        }
+
+        throw Exception("Could not get taf metar for icao $icao")
     }
 
 }
