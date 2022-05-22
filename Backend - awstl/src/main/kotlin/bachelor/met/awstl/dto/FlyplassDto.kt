9faf0f9424cd106925fb1bd803c286d004aec264
s341833 <56063595+s341833@users.noreply.github.com>
2022-05-22 15:12:31 +0200

    fix for new runway model

--- Backend - awstl/src/main/kotlin/bachelor/met/awstl/dto/FlyplassDto.kt
+++ Backend - awstl/src/main/kotlin/bachelor/met/awstl/dto/FlyplassDto.kt
@@ -1,7 +1,8 @@
 package bachelor.met.awstl.dto
 
+import bachelor.met.awstl.model.Rullebane
 import java.io.Serializable
 
-class FlyplassDto(var navn: String?, var icao: String, var rwy: String?): Serializable {
+class FlyplassDto(var navn: String?, var icao: String?, var rwy: Array<Rullebane>?): Serializable {
 
 }
